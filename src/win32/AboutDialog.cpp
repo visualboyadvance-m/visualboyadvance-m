a6574e784a7341e77c0c163c199937f52d04cf84
mudlord <mudlord@a31d4220-a93d-0410-bf67-fe4944624d44>
2007-11-12 21:18:39 +0000

    Updated about dialog with our site details as well as updated credits.

--- src/win32/AboutDialog.cpp
+++ src/win32/AboutDialog.cpp
@@ -69,7 +69,7 @@ BOOL AboutDialog::OnInitDialog()
     m_translator.SubclassDlgItem(IDC_TRANSLATOR_URL, this);
   }
 
-  m_link.SetWindowText("http://vba.ngemu.com");
+  m_link.SetWindowText("http://vbam.bountysource.com");
 
   return TRUE;  // return TRUE unless you set the focus to a control
   // EXCEPTION: OCX Property Pages should return FALSE
