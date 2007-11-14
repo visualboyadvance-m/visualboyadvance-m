5be56cdac5394b9ab6ed33315a011e991e76379a
Nach <Nach@a31d4220-a93d-0410-bf67-fe4944624d44>
2007-11-14 12:06:48 +0000

    Can now build under Linux, although it crashes very early.
    git-svn-id: https://svn.code.sf.net/p/vbam/code/trunk@67 a31d4220-a93d-0410-bf67-fe4944624d44

---
+++ src/expr.cpp.h
@@ -0,0 +1,13 @@
+#ifndef YYSTYPE
+#define YYSTYPE int
+#endif
+#define TOKEN_IDENTIFIER        257
+#define TOKEN_DOT       258
+#define TOKEN_STAR      259
+#define TOKEN_ARROW     260
+#define TOKEN_ADDR      261
+#define TOKEN_SIZEOF    262
+#define TOKEN_NUMBER    263
+
+
+extern YYSTYPE yylval;
