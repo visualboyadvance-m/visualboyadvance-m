8eaa904bc24cf3168015c37d88d213af53a8f49b
Nach <Nach@a31d4220-a93d-0410-bf67-fe4944624d44>
2007-11-15 01:29:11 +0000

    More portability using C99 types.
    git-svn-id: https://svn.code.sf.net/p/vbam/code/trunk@81 a31d4220-a93d-0410-bf67-fe4944624d44

--- src/System.h
+++ src/System.h
@@ -20,31 +20,27 @@
 #ifndef VBA_SYSTEM_H
 #define VBA_SYSTEM_H
 
+#include <stdint.h>
+
 #include "unzip.h"
 
 #ifndef NULL
 #define NULL 0
 #endif
 
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
 typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
-
-#ifdef _MSC_VER
-typedef unsigned __int64 u64;
-#else
-typedef unsigned long long u64;
-#endif
-
-typedef signed char s8;
-typedef signed short s16;
-typedef signed int s32;
-
-#ifdef _MSC_VER
-typedef signed __int64 s64;
-#else
-typedef signed long long s64;
-#endif
 
 struct EmulatedSystem {
   // main emulation function
