3f7a286dcc0421e70dce6d4593b3818ded42b523
spacy51 <spacy51@a31d4220-a93d-0410-bf67-fe4944624d44>
2007-12-23 21:09:29 +0000

    added NO_OGL switch simplified fall back when initializing render API failed

--- trunk/src/win32/OpenGL.cpp
+++ trunk/src/win32/OpenGL.cpp
@@ -17,6 +17,8 @@
 // along with this program; if not, write to the Free Software Foundation,
 // Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
+#ifndef NO_OGL
+
 #include "stdafx.h"
 
 #include "MainWnd.h"
@@ -684,3 +686,5 @@ IDisplay *newOpenGLDisplay()
 {
 	return new OpenGLDisplay();
 }
+
+#endif // #ifndef NO_OGL
