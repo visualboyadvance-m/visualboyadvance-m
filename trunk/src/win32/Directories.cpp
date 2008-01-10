7ce1b53acde251601747ab6b91014c52e6b099ba
spacy51 <spacy51@a31d4220-a93d-0410-bf67-fe4944624d44>
2008-01-10 17:04:21 +0000

    use newer (Windows 2000) version of folder selection

--- trunk/src/win32/Directories.cpp
+++ trunk/src/win32/Directories.cpp
@@ -267,7 +267,7 @@ CString Directories::browseForDir(CString title)
     bi.hwndOwner = m_hWnd;
     bi.lpszTitle = title;
     bi.pidlRoot = 0;
-    bi.ulFlags = BIF_RETURNONLYFSDIRS;
+    bi.ulFlags = BIF_RETURNONLYFSDIRS | BIF_EDITBOX | BIF_NEWDIALOGSTYLE;
     bi.lpfn = browseCallbackProc;
     bi.lParam = (LPARAM)(LPCTSTR)initialFolderDir;
 
