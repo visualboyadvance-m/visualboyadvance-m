0183f2a910319a428e7c68130422d9a5c7a56354
spacy51 <spacy51@a31d4220-a93d-0410-bf67-fe4944624d44>
2008-01-29 13:11:02 +0000

    ADDED compile date to about box REDESIGNED about box a little
    git-svn-id: https://svn.code.sf.net/p/vbam/code/trunk@345 a31d4220-a93d-0410-bf67-fe4944624d44

--- src/win32/AboutDialog.h
+++ src/win32/AboutDialog.h
@@ -2,6 +2,8 @@
 // VisualBoyAdvance - Nintendo Gameboy/GameboyAdvance (TM) emulator.
 // Copyright (C) 1999-2003 Forgotten
 // Copyright (C) 2004 Forgotten and the VBA development team
+// Copyright (C) 2008 VBA-M development team
+
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -17,56 +19,27 @@
 // along with this program; if not, write to the Free Software Foundation,
 // Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-#if !defined(AFX_ABOUTDIALOG_H__48D787B2_0699_4F03_827D_404EC70DDDB2__INCLUDED_)
-#define AFX_ABOUTDIALOG_H__48D787B2_0699_4F03_827D_404EC70DDDB2__INCLUDED_
-
-#if _MSC_VER > 1000
 #pragma once
-#endif // _MSC_VER > 1000
-// AboutDialog.h : header file
-//
+
 #include "stdafx.h"
 #include "Hyperlink.h"
 #include "resource.h"
 
-/////////////////////////////////////////////////////////////////////////////
-// AboutDialog dialog
 
 class AboutDialog : public CDialog
 {
-  Hyperlink m_link;
-  Hyperlink m_translator;
-  // Construction
- public:
-  AboutDialog(CWnd* pParent = NULL);   // standard constructor
+public:
+	AboutDialog(CWnd* pParent = NULL);
+	enum { IDD = IDD_ABOUT };
 
-  // Dialog Data
-  //{{AFX_DATA(AboutDialog)
-  enum { IDD = IDD_ABOUT };
-  CString  m_version;
-  //}}AFX_DATA
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);
+	virtual BOOL OnInitDialog();
+	DECLARE_MESSAGE_MAP()
 
-
-  // Overrides
-  // ClassWizard generated virtual function overrides
-  //{{AFX_VIRTUAL(AboutDialog)
- protected:
-  virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
-  //}}AFX_VIRTUAL
-
-  // Implementation
- protected:
-
-  // Generated message map functions
-  //{{AFX_MSG(AboutDialog)
-  virtual BOOL OnInitDialog();
-  //}}AFX_MSG
-
-
-  DECLARE_MESSAGE_MAP()
-    };
-
-    //{{AFX_INSERT_LOCATION}}
-    // Microsoft Visual C++ will insert additional declarations immediately before the previous line.
-
-#endif // !defined(AFX_ABOUTDIALOG_H__48D787B2_0699_4F03_827D_404EC70DDDB2__INCLUDED_)
+private:
+	Hyperlink m_link;
+	Hyperlink m_translator;
+	CString   m_version;
+	CString   m_date;
+};
