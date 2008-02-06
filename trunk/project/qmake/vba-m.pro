4ea961e4bdd2e2e2e928a4861671e15481a9933f
spacy51 <spacy51@a31d4220-a93d-0410-bf67-fe4944624d44>
2008-02-06 13:59:07 +0000

    ADDED basic Qt translation framework

--- trunk/project/qmake/vba-m.pro
+++ trunk/project/qmake/vba-m.pro
@@ -3,3 +3,4 @@ CONFIG += qt
 TARGET = VisualBoyAdvance
 HEADERS += ../../src/qt/main.h
 SOURCES += ../../src/qt/main.cpp
+TRANSLATIONS += ../../lang/german.ts
--- trunk/src/qt/main.cpp
+++ trunk/src/qt/main.cpp
@@ -2,13 +2,18 @@
 
 #include <QApplication>
 #include <QPushButton>
+#include <QTranslator>
 
 
 int main( int argc, char *argv[] )
 {
 	QApplication theApp( argc, argv );
 
-	QPushButton hello( TEST_STRING );
+	QTranslator translator;
+	translator.load( "lang/german" );
+	theApp.installTranslator( &translator );
+
+	QPushButton hello( QPushButton::tr( "Hello World!" ) );
 	hello.resize( 192, 48 );
 	hello.show();
 
