5fd75a955285a225e7e7e5b8bdafcdcfdca556ac
spacy51 <spacy51@a31d4220-a93d-0410-bf67-fe4944624d44>
2008-02-06 13:59:07 +0000

    ADDED basic Qt translation framework
    git-svn-id: https://svn.code.sf.net/p/vbam/code/trunk@353 a31d4220-a93d-0410-bf67-fe4944624d44

---
+++ lang/german.ts
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="de_DE">
+<context>
+    <name>QPushButton</name>
+    <message>
+        <location filename="../src/qt/main.cpp" line="16"/>
+        <source>Hello World!</source>
+        <translation>Hallo Welt!</translation>
+    </message>
+</context>
+</TS>
--- src/qt/main.h
+++ src/qt/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define TEST_STRING "Hello World!"
+
 
 #endif // #ifndef MAIN_H
