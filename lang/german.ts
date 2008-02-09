e2b89cf47371263845efc16aa82196734df6eb53
spacy51 <spacy51@a31d4220-a93d-0410-bf67-fe4944624d44>
2008-02-09 17:16:37 +0000

    QT: USE precompiled header ADDED basic menu ADDED OpenGL frame ADDED german translation
    git-svn-id: https://svn.code.sf.net/p/vbam/code/trunk@358 a31d4220-a93d-0410-bf67-fe4944624d44

--- lang/german.ts
+++ lang/german.ts
@@ -1,11 +1,97 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="de_DE">
+<defaultcodec></defaultcodec>
 <context>
-    <name>QPushButton</name>
+    <name>MainWnd</name>
     <message>
-        <location filename="../src/qt/main.cpp" line="16"/>
-        <source>Hello World!</source>
-        <translation>Hallo Welt!</translation>
+        <location filename="../src/qt/mainwnd.cpp" line="73"/>
+        <source>VBA-M</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/mainwnd.cpp" line="40"/>
+        <source>&amp;File</source>
+        <translation>&amp;Datei</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/mainwnd.cpp" line="41"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/mainwnd.cpp" line="42"/>
+        <source>&amp;Tools</source>
+        <translation>&amp;Werkzeuge</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/mainwnd.cpp" line="43"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Hilfe</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/mainwnd.cpp" line="45"/>
+        <source>About &amp;OpenGL...</source>
+        <translation>Über &amp;OpenGL...</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/mainwnd.cpp" line="49"/>
+        <source>About &amp;VBA-M...</source>
+        <translation>Über &amp;VBA-M...</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/mainwnd.cpp" line="53"/>
+        <source>About &amp;Qt...</source>
+        <translation>Über &amp;Qt...</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/mainwnd.cpp" line="74"/>
+        <source>This program is licensed under terms of the GNU General Public License.</source>
+        <translation>Dieses Programm ist unter den Bedingungen der GNU General Public License lizenziert.</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/mainwnd.cpp" line="88"/>
+        <source>OpenGL version 2.1 is present.</source>
+        <translation>OpenGL Version 2.1 ist verfügbar.</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/mainwnd.cpp" line="91"/>
+        <source>OpenGL version 2.0 is present.</source>
+        <translation>OpenGL Version 2.0 ist verfügbar.</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/mainwnd.cpp" line="94"/>
+        <source>OpenGL version 1.5 is present.</source>
+        <translation>OpenGL Version 1.5 ist verfügbar.</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/mainwnd.cpp" line="97"/>
+        <source>OpenGL version 1.4 is present.</source>
+        <translation>OpenGL Version 1.4 ist verfügbar.</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/mainwnd.cpp" line="100"/>
+        <source>OpenGL version 1.3 is present.</source>
+        <translation>OpenGL Version 1.3 ist verfügbar.</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/mainwnd.cpp" line="103"/>
+        <source>OpenGL version 1.2 is present.</source>
+        <translation>OpenGL Version 1.2 ist verfügbar.</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/mainwnd.cpp" line="106"/>
+        <source>OpenGL version 1.1 is present.</source>
+        <translation>OpenGL Version 1.1 ist verfügbar.</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/mainwnd.cpp" line="112"/>
+        <source>OpenGL is NOT available!</source>
+        <translation>OpenGL ist NICHT verfügbar!</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/mainwnd.cpp" line="115"/>
+        <source>OpenGL</source>
+        <translation></translation>
     </message>
 </context>
 </TS>
