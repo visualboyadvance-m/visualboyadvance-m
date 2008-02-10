7bf7416b660dcc1ec10b429756ee573d9fd1bbb2
spacy51 <spacy51@a31d4220-a93d-0410-bf67-fe4944624d44>
2008-02-10 12:35:36 +0000

    Qt: ADDED spanish translation ADDED switch to enabled/disable translation
    git-svn-id: https://svn.code.sf.net/p/vbam/code/trunk@363 a31d4220-a93d-0410-bf67-fe4944624d44

--- lang/german.ts
+++ lang/german.ts
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="de_DE">
-<defaultcodec></defaultcodec>
 <context>
     <name>@default</name>
     <message>
@@ -62,124 +61,154 @@
 <context>
     <name>MainWnd</name>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="35"/>
+        <location filename="../src/qt/MainWnd.cpp" line="36"/>
         <source>VBA-M</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="67"/>
+        <location filename="../src/qt/MainWnd.cpp" line="90"/>
         <source>&amp;File</source>
         <translation>&amp;Datei</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="71"/>
+        <location filename="../src/qt/MainWnd.cpp" line="95"/>
         <source>&amp;Settings</source>
         <translation>&amp;Einstellungen</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="75"/>
+        <location filename="../src/qt/MainWnd.cpp" line="101"/>
         <source>&amp;Tools</source>
         <translation>&amp;Werkzeuge</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="78"/>
+        <location filename="../src/qt/MainWnd.cpp" line="105"/>
         <source>&amp;Help</source>
         <translation>&amp;Hilfe</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="81"/>
+        <location filename="../src/qt/MainWnd.cpp" line="108"/>
         <source>About &amp;OpenGL...</source>
         <translation>Über &amp;OpenGL...</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="80"/>
+        <location filename="../src/qt/MainWnd.cpp" line="107"/>
         <source>About &amp;VBA-M...</source>
         <translation>Über &amp;VBA-M...</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="82"/>
+        <location filename="../src/qt/MainWnd.cpp" line="109"/>
         <source>About &amp;Qt...</source>
         <translation>Über &amp;Qt...</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="128"/>
+        <location filename="../src/qt/MainWnd.cpp" line="191"/>
         <source>This program is licensed under terms of the GNU General Public License.</source>
         <translation>Dieses Programm ist unter den Bedingungen der GNU General Public License lizenziert.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="142"/>
+        <location filename="../src/qt/MainWnd.cpp" line="205"/>
         <source>OpenGL version 2.1 is present.</source>
         <translation>OpenGL Version 2.1 ist verfügbar.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="145"/>
+        <location filename="../src/qt/MainWnd.cpp" line="208"/>
         <source>OpenGL version 2.0 is present.</source>
         <translation>OpenGL Version 2.0 ist verfügbar.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="148"/>
+        <location filename="../src/qt/MainWnd.cpp" line="211"/>
         <source>OpenGL version 1.5 is present.</source>
         <translation>OpenGL Version 1.5 ist verfügbar.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="151"/>
+        <location filename="../src/qt/MainWnd.cpp" line="214"/>
         <source>OpenGL version 1.4 is present.</source>
         <translation>OpenGL Version 1.4 ist verfügbar.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="154"/>
+        <location filename="../src/qt/MainWnd.cpp" line="217"/>
         <source>OpenGL version 1.3 is present.</source>
         <translation>OpenGL Version 1.3 ist verfügbar.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="157"/>
+        <location filename="../src/qt/MainWnd.cpp" line="220"/>
         <source>OpenGL version 1.2 is present.</source>
         <translation>OpenGL Version 1.2 ist verfügbar.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="160"/>
+        <location filename="../src/qt/MainWnd.cpp" line="223"/>
         <source>OpenGL version 1.1 is present.</source>
         <translation>OpenGL Version 1.1 ist verfügbar.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="166"/>
+        <location filename="../src/qt/MainWnd.cpp" line="229"/>
         <source>OpenGL is NOT available!</source>
         <translation>OpenGL ist NICHT verfügbar!</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="127"/>
+        <location filename="../src/qt/MainWnd.cpp" line="190"/>
         <source>About VBA-M</source>
         <translation>Über VBA-M</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="169"/>
+        <location filename="../src/qt/MainWnd.cpp" line="232"/>
         <source>About OpenGL</source>
         <translation>Über OpenGL</translation>
     </message>
     <message>
         <location filename="../src/qt/MainWnd.cpp" line="72"/>
         <source>Select translation...</source>
-        <translation>Übersetzung auswählen...</translation>
+        <translation type="obsolete">Übersetzung auswählen...</translation>
     </message>
     <message>
         <location filename="../src/qt/MainWnd.cpp" line="102"/>
         <source>Select translation</source>
-        <translation>Übersetzung auswählen</translation>
+        <translation type="obsolete">Übersetzung auswählen</translation>
     </message>
     <message>
         <location filename="../src/qt/MainWnd.cpp" line="104"/>
         <source>Translation files (*.qm)</source>
-        <translation>Übersetzungsdateien (*.qm)</translation>
+        <translation type="obsolete">Übersetzungsdateien (*.qm)</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="107"/>
+        <location filename="../src/qt/MainWnd.cpp" line="146"/>
         <source>.qm</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="68"/>
+        <location filename="../src/qt/MainWnd.cpp" line="91"/>
         <source>Exit</source>
         <translation>Beenden</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="96"/>
+        <source>Select language...</source>
+        <translation>Sprache auswählen...</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="129"/>
+        <source>Select language</source>
+        <translation>Sprache auswählen</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="131"/>
+        <source>Language files (*.qm)</source>
+        <translation>Sprachdateien (*.qm)</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="139"/>
+        <source>Error!</source>
+        <translation>Fehler!</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="139"/>
+        <source>Language file can not be loaded!</source>
+        <translation>Sprachdatei kann nicht geladen werden!</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="59"/>
+        <source>Enable translation</source>
+        <translation>Übersetzung aktivieren</translation>
     </message>
 </context>
 </TS>
