edb5c06f2c4f45c3260a2c706b35b639c831c280
mudlord <mudlord@a31d4220-a93d-0410-bf67-fe4944624d44>
2008-02-21 05:37:40 +0000

    Fixed some minor issues in the main dialog

--- src/qt/configdialog.cpp
+++ src/qt/configdialog.cpp
@@ -1,38 +1,19 @@
- /****************************************************************************
- **
- ** Copyright (C) 2005-2007 Trolltech ASA. All rights reserved.
- **
- ** This file is part of the example classes of the Qt Toolkit.
- **
- ** This file may be used under the terms of the GNU General Public
- ** License version 2.0 as published by the Free Software Foundation
- ** and appearing in the file LICENSE.GPL included in the packaging of
- ** this file.  Please review the following information to ensure GNU
- ** General Public Licensing requirements will be met:
- ** http://trolltech.com/products/qt/licenses/licensing/opensource/
- **
- ** If you are unsure which license is appropriate for your use, please
- ** review the following information:
- ** http://trolltech.com/products/qt/licenses/licensing/licensingoverview
- ** or contact the sales department at sales@trolltech.com.
- **
- ** In addition, as a special exception, Trolltech gives you certain
- ** additional rights. These rights are described in the Trolltech GPL
- ** Exception version 1.0, which can be found at
- ** http://www.trolltech.com/products/qt/gplexception/ and in the file
- ** GPL_EXCEPTION.txt in this package.
- **
- ** In addition, as a special exception, Trolltech, as the sole copyright
- ** holder for Qt Designer, grants users of the Qt/Eclipse Integration
- ** plug-in the right for the Qt/Eclipse Integration to link to
- ** functionality provided by Qt Designer and its related libraries.
- **
- ** Trolltech reserves all rights not expressly granted herein.
- **
- ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
- ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
- **
- ****************************************************************************/
+// VBA-M, A Nintendo Handheld Console Emulator
+// Copyright (C) 2008 VBA-M development team
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or(at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
  #include <QtGui>
 
@@ -43,9 +24,10 @@
  {
      contentsWidget = new QListWidget;
      contentsWidget->setViewMode(QListView::IconMode);
-     contentsWidget->setIconSize(QSize(96, 84));
+     contentsWidget->setIconSize(QSize(64, 64));
      contentsWidget->setMovement(QListView::Static);
-     contentsWidget->setMaximumWidth(128);
+     contentsWidget->setResizeMode(QListView::Adjust);
+     contentsWidget->setMaximumWidth(130);
      contentsWidget->setSpacing(12);
 
      pagesWidget = new QStackedWidget;
