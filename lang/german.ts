4070f2a9b39806b5fd0202b113aca9fe82b50dd1
spacy51 <spacy51@a31d4220-a93d-0410-bf67-fe4944624d44>
2008-02-23 20:13:48 +0000

    Qt: ADDED version info to about box
    git-svn-id: https://svn.code.sf.net/p/vbam/code/trunk@386 a31d4220-a93d-0410-bf67-fe4944624d44

--- lang/german.ts
+++ lang/german.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="de_DE">
+<defaultcodec></defaultcodec>
 <context>
     <name>@default</name>
     <message>
@@ -167,11 +168,6 @@
 <context>
     <name>MainWnd</name>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="38"/>
-        <source>VBA-M</source>
-        <translation></translation>
-    </message>
-    <message>
         <location filename="../src/qt/MainWnd.cpp" line="98"/>
         <source>&amp;File</source>
         <translation type="obsolete">&amp;Datei</translation>
@@ -207,57 +203,57 @@
         <translation type="obsolete">Über &amp;Qt...</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="226"/>
+        <location filename="../src/qt/MainWnd.cpp" line="285"/>
         <source>This program is licensed under terms of the GNU General Public License.</source>
         <translation>Dieses Programm ist unter den Bedingungen der GNU General Public License lizenziert.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="257"/>
+        <location filename="../src/qt/MainWnd.cpp" line="313"/>
         <source>OpenGL version 2.1 is present.</source>
         <translation>OpenGL Version 2.1 ist verfügbar.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="260"/>
+        <location filename="../src/qt/MainWnd.cpp" line="316"/>
         <source>OpenGL version 2.0 is present.</source>
         <translation>OpenGL Version 2.0 ist verfügbar.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="263"/>
+        <location filename="../src/qt/MainWnd.cpp" line="319"/>
         <source>OpenGL version 1.5 is present.</source>
         <translation>OpenGL Version 1.5 ist verfügbar.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="266"/>
+        <location filename="../src/qt/MainWnd.cpp" line="322"/>
         <source>OpenGL version 1.4 is present.</source>
         <translation>OpenGL Version 1.4 ist verfügbar.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="269"/>
+        <location filename="../src/qt/MainWnd.cpp" line="325"/>
         <source>OpenGL version 1.3 is present.</source>
         <translation>OpenGL Version 1.3 ist verfügbar.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="272"/>
+        <location filename="../src/qt/MainWnd.cpp" line="328"/>
         <source>OpenGL version 1.2 is present.</source>
         <translation>OpenGL Version 1.2 ist verfügbar.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="275"/>
+        <location filename="../src/qt/MainWnd.cpp" line="331"/>
         <source>OpenGL version 1.1 is present.</source>
         <translation>OpenGL Version 1.1 ist verfügbar.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="281"/>
+        <location filename="../src/qt/MainWnd.cpp" line="337"/>
         <source>OpenGL is NOT available!</source>
         <translation>OpenGL ist NICHT verfügbar!</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="231"/>
+        <location filename="../src/qt/MainWnd.cpp" line="287"/>
         <source>About VBA-M</source>
         <translation>Über VBA-M</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="284"/>
+        <location filename="../src/qt/MainWnd.cpp" line="340"/>
         <source>About OpenGL</source>
         <translation>Über OpenGL</translation>
     </message>
@@ -277,116 +273,131 @@
         <translation type="obsolete">Übersetzungsdateien (*.qm)</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="178"/>
+        <location filename="../src/qt/MainWnd.cpp" line="227"/>
         <source>.qm</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="99"/>
+        <location filename="../src/qt/MainWnd.cpp" line="138"/>
         <source>Exit</source>
         <translation>Beenden</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="105"/>
+        <location filename="../src/qt/MainWnd.cpp" line="144"/>
         <source>Select language...</source>
         <translation>Sprache auswählen...</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="160"/>
+        <location filename="../src/qt/MainWnd.cpp" line="206"/>
         <source>Select language</source>
         <translation>Sprache auswählen</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="162"/>
+        <location filename="../src/qt/MainWnd.cpp" line="208"/>
         <source>Language files (*.qm)</source>
         <translation>Sprachdateien (*.qm)</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="170"/>
+        <location filename="../src/qt/MainWnd.cpp" line="218"/>
         <source>Error!</source>
         <translation>Fehler!</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="170"/>
+        <location filename="../src/qt/MainWnd.cpp" line="218"/>
         <source>Language file can not be loaded!</source>
         <translation>Sprachdatei kann nicht geladen werden!</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="65"/>
+        <location filename="../src/qt/MainWnd.cpp" line="104"/>
         <source>Enable translation</source>
         <translation>Übersetzung aktivieren</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="229"/>
+        <location filename="../src/qt/MainWnd.cpp" line="280"/>
         <source>
 No language file loaded.</source>
-        <translation>
+        <translation type="obsolete">
 Deutsche Übersetzung von spacy.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="133"/>
+        <location filename="../src/qt/MainWnd.cpp" line="172"/>
         <source>Cheats</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="112"/>
+        <location filename="../src/qt/MainWnd.cpp" line="151"/>
         <source>Show cheats sidebar</source>
         <translation>Cheats-Seitenleiste anzeigen</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="97"/>
+        <location filename="../src/qt/MainWnd.cpp" line="136"/>
         <source>File</source>
         <translation>Datei</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="98"/>
+        <location filename="../src/qt/MainWnd.cpp" line="137"/>
         <source>Open ROM</source>
         <translation>ROM laden</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="103"/>
+        <location filename="../src/qt/MainWnd.cpp" line="142"/>
         <source>Settings</source>
         <translation>Einstellungen</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="104"/>
+        <location filename="../src/qt/MainWnd.cpp" line="143"/>
         <source>Main options...</source>
         <translation>Optionen...</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="110"/>
+        <location filename="../src/qt/MainWnd.cpp" line="149"/>
         <source>Tools</source>
         <translation>Werkzeuge</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="117"/>
+        <location filename="../src/qt/MainWnd.cpp" line="156"/>
         <source>Help</source>
         <translation>Hilfe</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="119"/>
+        <location filename="../src/qt/MainWnd.cpp" line="158"/>
         <source>About VBA-M...</source>
         <translation>Über VBA-M...</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="120"/>
+        <location filename="../src/qt/MainWnd.cpp" line="159"/>
         <source>About OpenGL...</source>
         <translation>Über OpenGL...</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="121"/>
+        <location filename="../src/qt/MainWnd.cpp" line="160"/>
         <source>About Qt...</source>
         <translation>Über Qt...</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="238"/>
+        <location filename="../src/qt/MainWnd.cpp" line="294"/>
         <source>Enter ROM loader code here.</source>
         <translation>Funktionalität noch nicht vorhanden.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="240"/>
+        <location filename="../src/qt/MainWnd.cpp" line="296"/>
         <source>Status</source>
         <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="278"/>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="280"/>
+        <source>Compile date:</source>
+        <translation>Bau-Datum:</translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="283"/>
+        <source>No language file loaded.</source>
+        <translation>Deutsche Übersetzung von spacy.</translation>
     </message>
 </context>
 <context>
@@ -420,12 +431,12 @@ Deutsche Übersetzung von spacy.</translation>
 <context>
     <name>sidewidget_cheats</name>
     <message>
-        <location filename="../src/qt/sidewidget_cheats.ui" line="25"/>
+        <location filename="../src/qt/sidewidget_cheats.ui" line="27"/>
         <source>Enable</source>
         <translation>Aktivieren</translation>
     </message>
     <message>
-        <location filename="../src/qt/sidewidget_cheats.ui" line="32"/>
+        <location filename="../src/qt/sidewidget_cheats.ui" line="34"/>
         <source>Search...</source>
         <translation>Suchen...</translation>
     </message>
