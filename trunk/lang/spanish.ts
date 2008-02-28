a94308ef00d429434b30e006dd9fda8ad9a9b8bf
Nach <Nach@a31d4220-a93d-0410-bf67-fe4944624d44>
2008-02-28 11:37:59 +0000

    Translation files update.

--- trunk/lang/spanish.ts
+++ trunk/lang/spanish.ts
@@ -1,26 +1,132 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="es">
 <context>
+    <name>ConfigDialog</name>
+    <message>
+        <location filename="../src/qt/configdialog.cpp" line="26"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/configdialog.cpp" line="62"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/configdialog.cpp" line="70"/>
+        <source>Video</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/configdialog.cpp" line="76"/>
+        <source>Sound</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/configdialog.cpp" line="82"/>
+        <source>Input</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InputOptionsPage</name>
+    <message>
+        <location filename="../src/qt/MainOptions.cpp" line="49"/>
+        <source>Input Keys</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainOptions.cpp" line="51"/>
+        <source>Start:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainOptions.cpp" line="54"/>
+        <source>Select:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainOptions.cpp" line="57"/>
+        <source>Up:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainOptions.cpp" line="60"/>
+        <source>Down:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainOptions.cpp" line="63"/>
+        <source>Left:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainOptions.cpp" line="66"/>
+        <source>Right:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainOptions.cpp" line="69"/>
+        <source>A:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainOptions.cpp" line="72"/>
+        <source>B:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainOptions.cpp" line="75"/>
+        <source>L:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainOptions.cpp" line="78"/>
+        <source>R:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainOptions.cpp" line="81"/>
+        <source>Gameshark:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainOptions.cpp" line="84"/>
+        <source>Speed Up:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainOptions.cpp" line="87"/>
+        <source>Screenshot:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainOptions.cpp" line="90"/>
+        <source>Multiple key assignments</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>MainWnd</name>
     <message>
         <location filename="../src/qt/MainWnd.cpp" line="38"/>
         <source>VBA-M</source>
-        <translation>VBA-M</translation>
+        <translation type="obsolete">VBA-M</translation>
     </message>
     <message>
         <location filename="../src/qt/MainWnd.cpp" line="93"/>
         <source>&amp;File</source>
-        <translation>&amp;Archivo</translation>
+        <translation type="obsolete">&amp;Archivo</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="94"/>
+        <location filename="../src/qt/MainWnd.cpp" line="158"/>
         <source>Exit</source>
         <translation>Salir</translation>
     </message>
     <message>
         <location filename="../src/qt/MainWnd.cpp" line="98"/>
         <source>&amp;Settings</source>
-        <translation>&amp;Ajustes</translation>
+        <translation type="obsolete">&amp;Ajustes</translation>
     </message>
     <message>
         <location filename="../src/qt/MainWnd.cpp" line="72"/>
@@ -30,27 +136,27 @@
     <message>
         <location filename="../src/qt/MainWnd.cpp" line="104"/>
         <source>&amp;Tools</source>
-        <translation>&amp;Herramientas</translation>
+        <translation type="obsolete">&amp;Herramientas</translation>
     </message>
     <message>
         <location filename="../src/qt/MainWnd.cpp" line="108"/>
         <source>&amp;Help</source>
-        <translation>&amp;Ayuda</translation>
+        <translation type="obsolete">&amp;Ayuda</translation>
     </message>
     <message>
         <location filename="../src/qt/MainWnd.cpp" line="110"/>
         <source>About &amp;VBA-M...</source>
-        <translation>Sobre &amp;VBA-M...</translation>
+        <translation type="obsolete">Sobre &amp;VBA-M...</translation>
     </message>
     <message>
         <location filename="../src/qt/MainWnd.cpp" line="111"/>
         <source>About &amp;OpenGL...</source>
-        <translation>Sobre &amp;OpenGL...</translation>
+        <translation type="obsolete">Sobre &amp;OpenGL...</translation>
     </message>
     <message>
         <location filename="../src/qt/MainWnd.cpp" line="112"/>
         <source>About &amp;Qt...</source>
-        <translation>Sobre &amp;Qt...</translation>
+        <translation type="obsolete">Sobre &amp;Qt...</translation>
     </message>
     <message>
         <location filename="../src/qt/MainWnd.cpp" line="102"/>
@@ -63,116 +169,203 @@
         <translation type="obsolete">Archivos de traducción (*.qm)</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="164"/>
+        <location filename="../src/qt/MainWnd.cpp" line="256"/>
         <source>.qm</source>
         <translation>.qm</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="215"/>
+        <location filename="../src/qt/MainWnd.cpp" line="316"/>
         <source>About VBA-M</source>
         <translation>Sobre VBA-M</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="210"/>
+        <location filename="../src/qt/MainWnd.cpp" line="314"/>
         <source>This program is licensed under terms of the GNU General Public License.</source>
         <translation>Este programa se licencia bajo los términos de la Licencia Pública General GNU.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="229"/>
+        <location filename="../src/qt/MainWnd.cpp" line="353"/>
         <source>OpenGL version 2.1 is present.</source>
         <translation>OpenGL versión 2.1 está presente.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="232"/>
+        <location filename="../src/qt/MainWnd.cpp" line="356"/>
         <source>OpenGL version 2.0 is present.</source>
         <translation>OpenGL versión 2.0 está presente.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="235"/>
+        <location filename="../src/qt/MainWnd.cpp" line="359"/>
         <source>OpenGL version 1.5 is present.</source>
         <translation>OpenGL versión 1.5 está presente.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="238"/>
+        <location filename="../src/qt/MainWnd.cpp" line="362"/>
         <source>OpenGL version 1.4 is present.</source>
         <translation>OpenGL versión 1.4 está presente.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="241"/>
+        <location filename="../src/qt/MainWnd.cpp" line="365"/>
         <source>OpenGL version 1.3 is present.</source>
         <translation>OpenGL versión 1.3 está presente.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="244"/>
+        <location filename="../src/qt/MainWnd.cpp" line="368"/>
         <source>OpenGL version 1.2 is present.</source>
         <translation>OpenGL versión 1.2 está presente.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="247"/>
+        <location filename="../src/qt/MainWnd.cpp" line="371"/>
         <source>OpenGL version 1.1 is present.</source>
         <translation>OpenGL versión 1.1 está presente.</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="253"/>
+        <location filename="../src/qt/MainWnd.cpp" line="377"/>
         <source>OpenGL is NOT available!</source>
         <translation>¡OpenGL no está disponible!</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="256"/>
+        <location filename="../src/qt/MainWnd.cpp" line="380"/>
         <source>About OpenGL</source>
         <translation>Sobre OpenGL</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="99"/>
+        <location filename="../src/qt/MainWnd.cpp" line="164"/>
         <source>Select language...</source>
         <translation>Seleccione traducción...</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="147"/>
+        <location filename="../src/qt/MainWnd.cpp" line="235"/>
         <source>Select language</source>
         <translation>Seleccione traducción</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="149"/>
+        <location filename="../src/qt/MainWnd.cpp" line="237"/>
         <source>Language files (*.qm)</source>
         <translation>Archivos de traducción (*.qm)</translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="157"/>
+        <location filename="../src/qt/MainWnd.cpp" line="332"/>
         <source>Error!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="157"/>
+        <location filename="../src/qt/MainWnd.cpp" line="247"/>
         <source>Language file can not be loaded!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="62"/>
+        <location filename="../src/qt/MainWnd.cpp" line="124"/>
         <source>Enable translation</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="213"/>
-        <source>
-No language file loaded.</source>
+        <location filename="../src/qt/MainWnd.cpp" line="192"/>
+        <source>Cheats</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qt/MainWnd.cpp" line="123"/>
-        <source>Cheats</source>
+        <location filename="../src/qt/MainWnd.cpp" line="156"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="157"/>
+        <source>Open ROM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="162"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="163"/>
+        <source>Main options...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="169"/>
+        <source>Tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="171"/>
+        <source>Show cheats sidebar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="176"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="178"/>
+        <source>About VBA-M...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="179"/>
+        <source>About OpenGL...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="180"/>
+        <source>About Qt...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="307"/>
+        <source>Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="309"/>
+        <source>Compile date:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="312"/>
+        <source>No language file loaded.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="324"/>
+        <source>Select ROM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="326"/>
+        <source>All ROMs (*.dmg *.sgb *.cgb *.agb *.gb *.gbc *.gba *.bin *.zip *.gz *.rar *.7z *.jma);;Game Boy ROMs (*.dmg *.gb);;Super Game Boy ROMs (*.sgb);;Game Boy Color ROMs (*.cgb *.gbc);;Game Boy Advance ROMs (*.agb *.gba);;Compressed ROMs (*.zip *.gz *.rar *.7z *.jma);;All Files (*.* *)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainWnd.cpp" line="332"/>
+        <source>Can not load ROM!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>VideoOptionsPage</name>
+    <message>
+        <location filename="../src/qt/MainOptions.cpp" line="24"/>
+        <source>Renderer Selection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qt/MainOptions.cpp" line="25"/>
+        <source>Renderer:</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>sidewidget_cheats</name>
     <message>
-        <location filename="../src/qt/sidewidget_cheats.ui" line="25"/>
+        <location filename="../src/qt/sidewidget_cheats.ui" line="27"/>
         <source>Enable</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qt/sidewidget_cheats.ui" line="32"/>
+        <location filename="../src/qt/sidewidget_cheats.ui" line="34"/>
         <source>Search...</source>
         <translation type="unfinished"></translation>
     </message>
