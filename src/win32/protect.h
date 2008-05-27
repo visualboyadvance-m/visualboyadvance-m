6b857fc253fd5d6c34627f80f6898a0f423bac34
n-a-c-h <n-a-c-h@a31d4220-a93d-0410-bf67-fe4944624d44>
2008-05-27 15:35:03 +0000

    Fixed unprotect_buffer().
    git-svn-id: https://svn.code.sf.net/p/vbam/code/trunk@536 a31d4220-a93d-0410-bf67-fe4944624d44

--- src/win32/protect.h
+++ src/win32/protect.h
@@ -8,7 +8,7 @@ extern "C" {
 //Returns 0 on success, 1 on failure, and <0 when an error occured
 //Note, can only be called once per execution
 int ExecutableValid(const char *executable_filename);
-void unprotect_buf(char *buffer, size_t buffer_len)
+char *unprotect_buffer(char *buffer, size_t buffer_len);
 
 #if defined(__cplusplus)
 }
