2de1c2928e264fb1810241f409ffc020c8f52bea
n-a-c-h <n-a-c-h@a31d4220-a93d-0410-bf67-fe4944624d44>
2008-05-29 03:20:05 +0000

    Fix for MSVC.

--- trunk/src/win32/protect.h
+++ trunk/src/win32/protect.h
@@ -10,13 +10,13 @@ extern "C" {
 int ExecutableValid(const char *executable_filename);
 char *unprotect_buffer(unsigned char *buffer, size_t buffer_len);
 
-#define SET_FN_PTR(func, num)                  \
-        static inline void *get_##func(void) { \
-          int  i, j = num / 4;                 \
-          long ptr = (long)func + num;         \
-          for (i = 0;  i < 2;  i++) ptr -= j;  \
-          return (void *)(ptr - (j * 2));      \
-        }
+#define SET_FN_PTR(func, num)             \
+ static __inline void *get_##func(void) { \
+  int  i, j = num / 4;                    \
+  long ptr = (long)func + num;            \
+  for (i = 0;  i < 2;  i++) { ptr -= j; } \
+  return (void *)(ptr - (j * 2));         \
+ }
 
 #define GET_FN_PTR(func) get_##func()
 
