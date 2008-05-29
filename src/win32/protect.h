05769eba134cd3d4524ecd739cecb07f2ebb9771
mudlord <mudlord@a31d4220-a93d-0410-bf67-fe4944624d44>
2008-05-29 04:05:38 +0000

    Fixed pragmas

--- src/win32/protect.h
+++ src/win32/protect.h
@@ -10,15 +10,16 @@ extern "C" {
 int ExecutableValid(const char *executable_filename);
 char *unprotect_buffer(unsigned char *buffer, size_t buffer_len);
 
+#pragma optimize("", off)
 #define SET_FN_PTR(func, num)              \
- #pragma optimize("", off)                 \
  static __inline void *get_##func(void) {  \
    int  i, j = num / 4;                    \
    long ptr = (long)func + num;            \
    for (i = 0;  i < 2;  i++) { ptr -= j; } \
    return (void *)(ptr - (j * 2));         \
  }                                         \
- #pragma optimize("", on)
+ 
+#pragma optimize("", on)
 
 #define GET_FN_PTR(func) get_##func()
 
