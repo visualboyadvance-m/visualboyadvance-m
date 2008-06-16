c64fd54be96be4ce2d20def165daad59f57952ce
bgk <bgk@a31d4220-a93d-0410-bf67-fe4944624d44>
2008-06-16 18:57:58 +0000

    Fix build without BKPT_SUPPORT

--- src/remote.cpp
+++ src/remote.cpp
@@ -467,6 +467,7 @@ void remoteWriteWatch(char *p, bool active)
     return;
   }
 
+#ifdef BKPT_SUPPORT
   for(int i = 0; i < count; i++) {
     if((address >> 24) == 2)
       freezeWorkRAM[address & 0x3ffff] = active;
@@ -474,6 +475,7 @@ void remoteWriteWatch(char *p, bool active)
       freezeInternalRAM[address & 0x7fff] = active;
     address++;
   }
+#endif
 
   remotePutPacket("OK");
 }
