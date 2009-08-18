d4219d9c0d4071b71422d2c48e8f909b4944b90f
spacy51 <spacy51@a31d4220-a93d-0410-bf67-fe4944624d44>
2009-08-18 20:32:26 +0000

    Add new command line switch: "--configpath YOURPATHHERE"

--- trunk/src/win32/Reg.h
+++ trunk/src/win32/Reg.h
@@ -10,7 +10,7 @@ void regSetStringValue(const char *key,const char *value);
 void regSetDwordValue(const char *key,DWORD value,bool force=false);
 void regSetBinaryValue(const char *key, char *value, int count);
 void regDeleteValue(char *key);
-void regInit(const char *);
+void regInit(const char *, bool force = false);
 void regShutdown();
 bool regCreateFileType( const char *ext, const char *type );
 bool regAssociateType( const char *type, const char *desc, const char *application, const char *icon = NULL );
