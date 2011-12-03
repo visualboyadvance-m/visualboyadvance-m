15767ed9bff74eed009c9ef4f8e158ff9f88a05b
bgk <bgk@a31d4220-a93d-0410-bf67-fe4944624d44>
2011-12-03 20:29:05 +0000

    NLS: Improve translations build
    - wxWidgets translations now work the same as GTK translations.
    - Translations for WX and GTK can now be generated at the same time.

--- trunk/po/update_pot.sh
+++ trunk/po/update_pot.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Generate translation template file for the GTK+ port
 intltool-extract --local --type=gettext/glade ../src/gtk/ui/cheatedit.ui
 intltool-extract --local --type=gettext/glade ../src/gtk/ui/cheatlist.ui
 intltool-extract --local --type=gettext/glade ../src/gtk/ui/display.ui
@@ -9,6 +10,13 @@ intltool-extract --local --type=gettext/glade ../src/gtk/ui/preferences.ui
 intltool-extract --local --type=gettext/glade ../src/gtk/ui/sound.ui
 intltool-extract --local --type=gettext/glade ../src/gtk/ui/vbam.ui
 
-xgettext -k_ -kN_ -o gvbam.pot ../src/gtk/*.cpp tmp/*.h
+xgettext -k_ -kN_ -o gvbam/gvbam.pot ../src/gtk/*.cpp tmp/*.h
 
 rm -r tmp/
+
+# Generate translation template file for the wxWidgets port
+wxrc -g ../src/wx/wxvbam.xrc -o wx-xrc-strings.h
+
+xgettext -k_ -kN_ -o wxvbam/wxvbam.pot ../src/wx/*.cpp ../src/wx/*.h ../src/wx/widgets/*.cpp wx-xrc-strings.h
+
+rm -r wx-xrc-strings.h
