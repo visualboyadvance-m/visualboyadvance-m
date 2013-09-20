a1306398eea0aa5d6f83343430af306a4d79d77d
thibeaz <thibeaz@a31d4220-a93d-0410-bf67-fe4944624d44>
2013-09-20 21:08:23 +0000

    libretro merge into trunk, the individual interfaces can still be compiled on their own so the gtk/sdl/mfc interfaces can still work as originally intended. Special thanks to Squarepusher of the libretro team for helping us get this merged

--- trunk/src/gba/RTC.h
+++ trunk/src/gba/RTC.h
@@ -7,7 +7,12 @@ void rtcEnable(bool);
 bool rtcIsEnabled();
 void rtcReset();
 
+#ifdef __LIBRETRO__
+void rtcReadGame(const u8 *&data);
+void rtcSaveGame(u8 *&data);
+#else
 void rtcReadGame(gzFile gzFile);
 void rtcSaveGame(gzFile gzFile);
+#endif
 
 #endif // RTC_H
