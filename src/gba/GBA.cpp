0e4e1c2dd62e8f6136200cb7f78a4bdc99831d29
squall-leonhart <squall-leonhart@a31d4220-a93d-0410-bf67-fe4944624d44>
2013-09-21 02:42:37 +0000

    revert 1207-1210 as its not really ready for integration yet.
    git-svn-id: https://svn.code.sf.net/p/vbam/code/trunk@1211 a31d4220-a93d-0410-bf67-fe4944624d44

--- src/gba/GBA.cpp
+++ src/gba/GBA.cpp
@@ -577,44 +577,6 @@ void CPUUpdateRenderBuffers(bool force)
   }
 }
 
-#ifdef __LIBRETRO__
-#include <cstddef>
-
-unsigned int CPUWriteState(u8* data, unsigned size)
-{
-   uint8_t *orig = data;
-
-   utilWriteIntMem(data, SAVE_GAME_VERSION);
-   utilWriteMem(data, &rom[0xa0], 16);
-   utilWriteIntMem(data, useBios);
-   utilWriteMem(data, &reg[0], sizeof(reg));
-
-   utilWriteDataMem(data, saveGameStruct);
-
-   utilWriteIntMem(data, stopState);
-   utilWriteIntMem(data, IRQTicks);
-
-   utilWriteMem(data, internalRAM, 0x8000);
-   utilWriteMem(data, paletteRAM, 0x400);
-   utilWriteMem(data, workRAM, 0x40000);
-   utilWriteMem(data, vram, 0x20000);
-   utilWriteMem(data, oam, 0x400);
-   utilWriteMem(data, pix, 4 * 241 * 162);
-   utilWriteMem(data, ioMem, 0x400);
-
-   eepromSaveGame(data);
-   flashSaveGame(data);
-   soundSaveGame(data);
-   rtcSaveGame(data);
-
-   return (ptrdiff_t)data - (ptrdiff_t)orig;
-}
-
-bool CPUWriteMemState(char *memory, int available)
-{
-   return false;
-}
-#else
 static bool CPUWriteState(gzFile gzFile)
 {
   utilWriteInt(gzFile, SAVE_GAME_VERSION);
@@ -668,7 +630,6 @@ bool CPUWriteState(const char *file)
   return res;
 }
 
-
 bool CPUWriteMemState(char *memory, int available)
 {
   gzFile gzFile = utilMemGzOpen(memory, available, "w");
@@ -688,108 +649,7 @@ bool CPUWriteMemState(char *memory, int available)
 
   return res;
 }
-#endif
 
-
-#ifdef __LIBRETRO__
-bool CPUReadState(const u8* data, unsigned size)
-{
-   // Don't really care about version.
-   int version = utilReadIntMem(data);
-   if (version != SAVE_GAME_VERSION)
-      return false;
-
-   char romname[16];
-   utilReadMem(romname, data, 16);
-   if (memcmp(&rom[0xa0], romname, 16) != 0)
-      return false;
-
-   // Don't care about use bios ...
-   utilReadIntMem(data);
-
-   utilReadMem(&reg[0], data, sizeof(reg));
-
-   utilReadDataMem(data, saveGameStruct);
-
-   stopState = utilReadIntMem(data) ? true : false;
-
-   IRQTicks = utilReadIntMem(data);
-   if (IRQTicks > 0)
-      intState = true;
-   else
-   {
-      intState = false;
-      IRQTicks = 0;
-   }
-
-   utilReadMem(internalRAM, data, 0x8000);
-   utilReadMem(paletteRAM, data, 0x400);
-   utilReadMem(workRAM, data, 0x40000);
-   utilReadMem(vram, data, 0x20000);
-   utilReadMem(oam, data, 0x400);
-   utilReadMem(pix, data, 4*241*162);
-   utilReadMem(ioMem, data, 0x400);
-
-   eepromReadGame(data, version);
-   flashReadGame(data, version);
-   soundReadGame(data, version);
-   rtcReadGame(data);
-
-   //// Copypasta stuff ...
-   // set pointers!
-   layerEnable = layerSettings & DISPCNT;
-
-   CPUUpdateRender();
-
-   // CPU Update Render Buffers set to true
-   CLEAR_ARRAY(line0);
-   CLEAR_ARRAY(line1);
-   CLEAR_ARRAY(line2);
-   CLEAR_ARRAY(line3);
-   // End of CPU Update Render Buffers set to true
-
-   CPUUpdateWindow0();
-   CPUUpdateWindow1();
-   gbaSaveType = 0;
-   switch(saveType) {
-      case 0:
-         cpuSaveGameFunc = flashSaveDecide;
-         break;
-      case 1:
-         cpuSaveGameFunc = sramWrite;
-         gbaSaveType = 1;
-         break;
-      case 2:
-         cpuSaveGameFunc = flashWrite;
-         gbaSaveType = 2;
-         break;
-      case 3:
-         break;
-      case 5:
-         gbaSaveType = 5;
-         break;
-      default:
-#ifdef CELL_VBA_DEBUG
-         systemMessage(MSG_UNSUPPORTED_SAVE_TYPE,
-               N_("Unsupported save type %d"), saveType);
-#endif
-         break;
-   }
-   if(eepromInUse)
-      gbaSaveType = 3;
-
-   systemSaveUpdateCounter = SYSTEM_SAVE_NOT_UPDATED;
-   if(armState) {
-      ARM_PREFETCH;
-   } else {
-      THUMB_PREFETCH;
-   }
-
-   CPUUpdateRegister(0x204, CPUReadHalfWordQuick(0x4000204));
-
-   return true;
-}
-#else
 static bool CPUReadState(gzFile gzFile)
 {
   int version = utilReadInt(gzFile);
@@ -986,7 +846,6 @@ bool CPUReadState(const char * file)
 
   return res;
 }
-#endif
 
 bool CPUExportEepromFile(const char *fileName)
 {
@@ -4140,11 +3999,7 @@ struct EmulatedSystem GBASystem = {
   // emuWriteState
   CPUWriteState,
   // emuReadMemState
-#ifdef __LIBRETRO__
-  NULL,
-#else
   CPUReadMemState,
-#endif
   // emuWriteMemState
   CPUWriteMemState,
   // emuWritePNG
