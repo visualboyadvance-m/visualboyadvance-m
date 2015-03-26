3d22df7e760f9bc60d7b3b21d9770c45c45cdfa2
skidau <skidau@a31d4220-a93d-0410-bf67-fe4944624d44>
2015-03-26 21:19:37 +0000

    Added VS2013 solution by mickdermack.
    Have to install the Multibyte MFC Library for Visual Studio 2013, because Multibyte strings were deprecated in favor of wchar strings.
    
    https://www.microsoft.com/download/details.aspx?id=40770
    
    git-svn-id: https://svn.code.sf.net/p/vbam/code/trunk@1247 a31d4220-a93d-0410-bf67-fe4944624d44

--- src/win32/targetver.h
+++ src/win32/targetver.h
@@ -7,12 +7,12 @@
 
 // Modify the following defines if you have to target a platform prior to the ones specified below.
 // Refer to MSDN for the latest info on corresponding values for different platforms.
-#ifndef WINVER                  // Specifies that the minimum required platform is Windows 2000.
-#define WINVER 0x0500           // Change this to the appropriate value to target other versions of Windows.
+#ifndef WINVER                  // Specifies that the minimum required platform is Windows XP.
+#define WINVER 0x0501           // Change this to the appropriate value to target other versions of Windows.
 #endif
 
-#ifndef _WIN32_WINNT            // Specifies that the minimum required platform is Windows 2000.
-#define _WIN32_WINNT 0x0500     // Change this to the appropriate value to target other versions of Windows.
+#ifndef _WIN32_WINNT            // Specifies that the minimum required platform is Windows XP.
+#define _WIN32_WINNT 0x0501     // Change this to the appropriate value to target other versions of Windows.
 #endif
 
 #ifndef _WIN32_WINDOWS          // Specifies that the minimum required platform is Windows 98.
