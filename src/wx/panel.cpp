2a6ba9b0dad8d6d34b0b201ab4d47d852d151728
Mystro256 <alexjnewt@hotmail.com>
2017-01-30 00:25:38 -0500

    Queue refresh after drawing (required for wayland)

--- src/wx/panel.cpp
+++ src/wx/panel.cpp
@@ -1402,6 +1402,10 @@ void DrawingPanelBase::PaintEv(wxPaintEvent& ev)
 
     DrawArea(dc);
     DrawOSD(dc);
+#ifndef NO_CAIRO
+    if (!paused)
+        GetWindow()->Refresh();
+#endif
 }
 
 void DrawingPanelBase::EraseBackground(wxEraseEvent& ev)
