015e0af857dc558d901fd5584793b994aa85343e
Zach Bacon <zachbacon@budgiemedia.rocks>
2017-03-09 03:58:00 -0500

    Start removing ancient history of gvbam, may it rest in peace

--- po/update_pot.sh
+++ po/update_pot.sh
@@ -1,19 +1,4 @@
 #!/bin/sh
-
-# Generate translation template file for the GTK+ port
-intltool-extract --local --type=gettext/glade ../src/gtk/ui/cheatedit.ui
-intltool-extract --local --type=gettext/glade ../src/gtk/ui/cheatlist.ui
-intltool-extract --local --type=gettext/glade ../src/gtk/ui/display.ui
-intltool-extract --local --type=gettext/glade ../src/gtk/ui/gameboyadvance.ui
-intltool-extract --local --type=gettext/glade ../src/gtk/ui/gameboy.ui
-intltool-extract --local --type=gettext/glade ../src/gtk/ui/preferences.ui
-intltool-extract --local --type=gettext/glade ../src/gtk/ui/sound.ui
-intltool-extract --local --type=gettext/glade ../src/gtk/ui/vbam.ui
-
-xgettext -k_ -kN_ -o gvbam/gvbam.pot ../src/gtk/*.cpp tmp/*.h
-
-rm -r tmp/
-
 # Generate translation template file for the wxWidgets port
 wxrc -g ../src/wx/xrc/*.xrc -o wx-xrc-strings.h
 
