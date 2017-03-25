5d4ce82d544ac1cd104135b2541ccc2f9ad836a2
Rafael Kitover <rkitover@gmail.com>
2017-03-25 03:58:30 -0700

    make bins built with asm disable exec stack #98
    Add the `section .note.GNU-stack` markers to .asm files built with nasm
    to make sure the final executables do not have an executable stack.
    
    This is described here:
    
    https://wiki.gentoo.org/wiki/Hardened/GNU_stack_quickstart

--- src/filters/2xSaImmx.asm
+++ src/filters/2xSaImmx.asm
@@ -24,7 +24,15 @@
 
 ;%define FAR_POINTER
 
-
+%ifidn __OUTPUT_FORMAT__,elf
+section .note.GNU-stack noalloc noexec nowrite progbits
+%endif
+%ifidn __OUTPUT_FORMAT__,elf32
+section .note.GNU-stack noalloc noexec nowrite progbits
+%endif
+%ifidn __OUTPUT_FORMAT__,elf64
+section .note.GNU-stack noalloc noexec nowrite progbits
+%endif
 
 BITS 32
 GLOBAL __2xSaILine
