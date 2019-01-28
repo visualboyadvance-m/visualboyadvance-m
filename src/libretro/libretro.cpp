b236d5fcc13891b3cec2a53e08f4eb47dac01e8d
JackoboLeChocobo <42393934+JackoboLeChocobo@users.noreply.github.com>
2019-01-28 16:09:04 +0100

    Libretro : Core Update
    Fixes for update_variables() saturation and add #define blocs for Link compilation

--- src/libretro/libretro.cpp
+++ src/libretro/libretro.cpp
@@ -508,6 +508,7 @@ void retro_set_environment(retro_environment_t cb)
       { "vbam_sound_4", "Sound channel 4; enabled|disabled" },
       { "vbam_sound_5", "Direct Sound A; enabled|disabled" },
       { "vbam_sound_6", "Direct Sound B; enabled|disabled" },
+#ifndef NO_LINK
       { "vbam_link_mode", "GBA Mode (needs restart); Not Connected|2P Network Server|3P Network Server|4P Network Server|Network Client" },
       { "vbam_link_mode_type", "GBA Link type (needs restart); GB/GBC|GBA" },	  
       { "vbam_link_timeout", "Timeout (ms) (needs restart); 0|50|100|150|200|250|300|350|400|450|500|550|600|650|700|750|800|850|900|950|1000" },
@@ -515,6 +516,7 @@ void retro_set_environment(retro_environment_t cb)
       { "vbam_link_network_server_ip_octet2", "Network link server address part 2 (client only) (needs restart); 0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37|38|39|40|41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|56|57|58|59|60|61|62|63|64|65|66|67|68|69|70|71|72|73|74|75|76|77|78|79|80|81|82|83|84|85|86|87|88|89|90|91|92|93|94|95|96|97|98|99|100|101|102|103|104|105|106|107|108|109|110|111|112|113|114|115|116|117|118|119|120|121|122|123|124|125|126|127|128|129|130|131|132|133|134|135|136|137|138|139|140|141|142|143|144|145|146|147|148|149|150|151|152|153|154|155|156|157|158|159|160|161|162|163|164|165|166|167|168|169|170|171|172|173|174|175|176|177|178|179|180|181|182|183|184|185|186|187|188|189|190|191|192|193|194|195|196|197|198|199|200|201|202|203|204|205|206|207|208|209|210|211|212|213|214|215|216|217|218|219|220|221|222|223|224|225|226|227|228|229|230|231|232|233|234|235|236|237|238|239|240|241|242|243|244|245|246|247|248|249|250|251|252|253|254|255" },
       { "vbam_link_network_server_ip_octet3", "Network link server address part 3 (client only) (needs restart); 0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37|38|39|40|41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|56|57|58|59|60|61|62|63|64|65|66|67|68|69|70|71|72|73|74|75|76|77|78|79|80|81|82|83|84|85|86|87|88|89|90|91|92|93|94|95|96|97|98|99|100|101|102|103|104|105|106|107|108|109|110|111|112|113|114|115|116|117|118|119|120|121|122|123|124|125|126|127|128|129|130|131|132|133|134|135|136|137|138|139|140|141|142|143|144|145|146|147|148|149|150|151|152|153|154|155|156|157|158|159|160|161|162|163|164|165|166|167|168|169|170|171|172|173|174|175|176|177|178|179|180|181|182|183|184|185|186|187|188|189|190|191|192|193|194|195|196|197|198|199|200|201|202|203|204|205|206|207|208|209|210|211|212|213|214|215|216|217|218|219|220|221|222|223|224|225|226|227|228|229|230|231|232|233|234|235|236|237|238|239|240|241|242|243|244|245|246|247|248|249|250|251|252|253|254|255" },
       { "vbam_link_network_server_ip_octet4", "Network link server address part 4 (client only) (needs restart); 0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37|38|39|40|41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|56|57|58|59|60|61|62|63|64|65|66|67|68|69|70|71|72|73|74|75|76|77|78|79|80|81|82|83|84|85|86|87|88|89|90|91|92|93|94|95|96|97|98|99|100|101|102|103|104|105|106|107|108|109|110|111|112|113|114|115|116|117|118|119|120|121|122|123|124|125|126|127|128|129|130|131|132|133|134|135|136|137|138|139|140|141|142|143|144|145|146|147|148|149|150|151|152|153|154|155|156|157|158|159|160|161|162|163|164|165|166|167|168|169|170|171|172|173|174|175|176|177|178|179|180|181|182|183|184|185|186|187|188|189|190|191|192|193|194|195|196|197|198|199|200|201|202|203|204|205|206|207|208|209|210|211|212|213|214|215|216|217|218|219|220|221|222|223|224|225|226|227|228|229|230|231|232|233|234|235|236|237|238|239|240|241|242|243|244|245|246|247|248|249|250|251|252|253|254|255" },
+#endif
       { NULL, NULL },
    };
 
@@ -969,7 +971,7 @@ static int astick_deadzone;
 static int gyro_sensitivity, tilt_sensitivity;
 static bool swap_astick;
 
-static void update_variables(bool startup)
+static void update_variables()
 {
     bool sound_changed = false;
     char key[256];
@@ -1065,8 +1067,7 @@ static void update_variables(bool startup)
             gbBorderAutomatic = 0;
         }
 
-        if ((type == IMAGE_GB) &&
-            (oldval != ((gbBorderOn << 1) | gbBorderAutomatic)) && !startup) {
+        if ((type == IMAGE_GB) && (oldval != ((gbBorderOn << 1) | gbBorderAutomatic))) {
             if (gbBorderOn) {
                 systemGbBorderOn();
                 gbSgbRenderBorder();
@@ -1184,7 +1185,8 @@ static void update_variables(bool startup)
 		gbSerialFunction = gbPrinterSend;
    else
 		gbSerialFunction = NULL;
-   
+
+#ifndef NO_LINK   
    var.key = "vbam_link_mode";
    if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value) {
       if (!strcmp(var.value, "Not Connected")) {
@@ -1283,7 +1285,8 @@ static void update_variables(bool startup)
 		}
 	}
 	
-	config_link = true;
+   config_link = true;
+#endif
 }
 
 // System analog stick range is -0x7fff to 0x7fff
@@ -1380,7 +1383,7 @@ void retro_run(void)
     bool updated = false;
 
     if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE_UPDATE, &updated) && updated)
-        update_variables(false);
+        update_variables();
 
     poll_cb();
 
@@ -1517,6 +1520,8 @@ static void update_input_descriptors(void)
 
 bool retro_load_game(const struct retro_game_info *game)
 {
+   update_variables();
+
    type = utilFindType((const char *)game->path);
 
    if (type == IMAGE_UNKNOWN) {
@@ -1524,7 +1529,6 @@ bool retro_load_game(const struct retro_game_info *game)
       return false;
    }
 
-   update_variables(true);
    update_colormaps();
    soundInit();
 
@@ -1618,7 +1622,6 @@ bool retro_load_game(const struct retro_game_info *game)
        return false;
 
    update_input_descriptors();    // Initialize input descriptors and info
-   update_variables(false);
    uint8_t* state_buf = (uint8_t*)malloc(2000000);
    serialize_size = core->emuWriteState(state_buf, 2000000);
    free(state_buf);
