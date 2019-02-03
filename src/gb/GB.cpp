visualboyadvance-m/visualboyadvance-m

commit 16dd5d4068cd2f5863a57599604a0eb63511be14

make speedup/turbo configurable + misc #339
Add Speedup / Turbo configuration panel which allows setting the
throttle or number of frames to skip for when the speed key is pressed
or turbo is enabled (which just presses the speed key.)

Throttle and frame-skip are mutually exclusive, throttle must be 0 (no
throttle) when number of frames to skip is non-zero. The dialog controls
handle this.

This is implemented in the core in GBA.cpp, GB.cpp and ConfigManager.

Two new options are added both in ConfigManager and in the wx options,
speedup_throttle and speedup_frame_skip, the defaults are:

```
speedup_throttle   = 0 (no throttle)
speedup_frame_skip = 9
```

this was the original behavior.

Add support for unsigned ints to wx/opts.cpp for these and for throttle,
this requires a new validator wxUIntValidator to use them in spin
controls.

Clean up appearance of the throttle spin control in the General dialog.

Maximum throttle and speedup_throttle is 600, values much over 500 will
not behave differently from 0 on modern hardware.

Maximum frame skip is 30 at the moment.

Signed-off-by: Rafael Kitover <rkitover@gmail.com>

src/gb/GB.cpp
@@ -10,6 +10,7 @@
 #include "../Util.h"
 #include "../common/ConfigManager.h"
 #include "../gba/GBALink.h"
+#include "../gba/Sound.h"
 #include "gb.h"
 #include "gbCheats.h"
 #include "gbGlobals.h"
@@ -4842,8 +4843,32 @@ void gbEmulate(int ticksToStop)
 
                 if ((gbLcdTicksDelayed <= 0) && (gbLCDChangeHappened)) {
                     int framesToSkip = systemFrameSkip;
-                    if (speedup)
-                        framesToSkip = 9; // try 6 FPS during speedup
+                    static bool speedup_throttle_set = false;
+                    static uint32_t last_throttle;
+
+                    if ((gbJoymask[0] >> 10) & 1) {
+                        if (speedup_throttle != 0) {
+                            if (!speedup_throttle_set && throttle != speedup_throttle) {
+                                last_throttle = throttle;
+                                throttle = speedup_throttle;
+                                soundSetThrottle(speedup_throttle);
+                                speedup_throttle_set = true;
+                            }
+                        }
+                        else {
+                            if (speedup_frame_skip)
+                                framesToSkip = speedup_frame_skip;
+
+                            speedup_throttle_set = false;
+                        }
+                    }
+                    else if (speedup_throttle_set) {
+                        throttle = last_throttle;
+                        soundSetThrottle(last_throttle);
+
+                        speedup_throttle_set = false;
+                    }
+
                     //gbLcdTicksDelayed = gbLcdTicks+1;
                     gbLCDChangeHappened = false;
                     switch (gbLcdModeDelayed) {
@@ -4919,7 +4944,11 @@ void gbEmulate(int ticksToStop)
 
                             newmask = (gbJoymask[0] >> 10);
 
-                            speedup = (newmask & 1) ? true : false;
+                            speedup = false;
+
+                            if (newmask & 1 && speedup_throttle == 0)
+                                speedup = true;
+
                             gbCapture = (newmask & 2) ? true : false;
 
                             if (gbCapture && !gbCapturePrevious) {

src/gba/GBA.cpp
@@ -3756,8 +3756,31 @@ void CPULoop(int ticks)
                     }
                 } else {
                     int framesToSkip = systemFrameSkip;
-                    if (speedup)
-                        framesToSkip = 9; // try 6 FPS during speedup
+                    static bool speedup_throttle_set = false;
+                    static uint32_t last_throttle;
+
+                    if ((joy >> 10) & 1) {
+                        if (speedup_throttle != 0) {
+                            if (!speedup_throttle_set && throttle != speedup_throttle) {
+                                last_throttle = throttle;
+                                throttle = speedup_throttle;
+                                soundSetThrottle(speedup_throttle);
+                                speedup_throttle_set = true;
+                            }
+                        }
+                        else {
+                            if (speedup_frame_skip)
+                                framesToSkip = speedup_frame_skip;
+
+                            speedup_throttle_set = false;
+                        }
+                    }
+                    else if (speedup_throttle_set) {
+                        throttle = last_throttle;
+                        soundSetThrottle(last_throttle);
+
+                        speedup_throttle_set = false;
+                    }
 
                     if (DISPSTAT & 2) {
                         // if in H-Blank, leave it and move to drawing mode
@@ -3788,7 +3811,12 @@ void CPULoop(int ticks)
                             // If no (m) code is enabled, apply the cheats at each LCDline
                             if ((cheatsEnabled) && (mastercode == 0))
                                 remainingTicks += cheatsCheckKeys(P1 ^ 0x3FF, ext);
-                            speedup = (ext & 1) ? true : false;
+
+                            speedup = false;
+
+                            if (ext & 1 && speedup_throttle == 0)
+                                speedup = true;
+
                             capture = (ext & 2) ? true : false;
 
                             if (capture && !capturePrevious) {

src/common/ConfigManager.cpp
@@ -104,8 +104,9 @@ enum named_opts
 	OPT_THREAD_PRIORITY,
 	OPT_VIDEO_OPTION,
 	OPT_WINDOW_POSITION_X,
-	OPT_WINDOW_POSITION_Y
-
+	OPT_WINDOW_POSITION_Y,
+	OPT_SPEEDUP_THROTTLE,
+	OPT_SPEEDUP_FRAME_SKIP
 };
 
 #define SOUND_MAX_VOLUME 2.0
@@ -242,7 +243,9 @@ int* rewindSerials = NULL;
 uint32_t autoFrameSkipLastTime;
 uint32_t movieLastJoypad;
 uint32_t movieNextJoypad;
-int throttle;
+uint32_t throttle = 100;
+uint32_t speedup_throttle = 0;
+uint32_t speedup_frame_skip = 9;
 
 const char* preparedCheatCodes[MAX_CHEATS];
 
@@ -374,6 +377,8 @@ struct option argOptions[] = {
 	{ "synchronize", required_argument, 0, OPT_SYNCHRONIZE },
 	{ "thread-priority", required_argument, 0, OPT_THREAD_PRIORITY },
 	{ "throttle", required_argument, 0, 'T' },
+	{ "speedup_throttle", required_argument, 0, OPT_SPEEDUP_THROTTLE },
+	{ "speedup_frame_skip", required_argument, 0, OPT_SPEEDUP_FRAME_SKIP },
 	{ "triple-buffering", no_argument, &tripleBuffering, 1 },
 	{ "use-bios", no_argument, &useBios, 1 },
 	{ "use-bios-file-gb", no_argument, &useBiosFileGB, 1 },
@@ -527,6 +532,8 @@ void LoadConfig()
 	soundRecordDir = ReadPrefString("soundRecordDir");
 	threadPriority = ReadPref("priority", 2);
 	throttle = ReadPref("throttle", 100);
+        speedup_throttle = ReadPref("speedup_throttle", 0);
+        speedup_frame_skip = ReadPref("speedup_frame_skip", 9);
 	tripleBuffering = ReadPref("tripleBuffering", 0);
 	useBios = ReadPrefHex("useBiosGBA");
 	useBiosFileGB = ReadPref("useBiosGB", 0);
@@ -971,6 +978,10 @@ int ReadOpts(int argc, char ** argv)
 				filter = kStretch2x;
 			}
 			break;
+                case 'T':
+                        if (optarg)
+                            throttle = atoi(optarg);
+                        break;
 		case 'I':
 			if (optarg) {
 				ifbType = (IFBFilter)atoi(optarg);
@@ -1339,6 +1350,14 @@ int ReadOpts(int argc, char ** argv)
 			// --dotcode-file-name-save
 			saveDotCodeFile = optarg;
 			break;
+                case OPT_SPEEDUP_THROTTLE:
+                        if (optarg)
+                            speedup_throttle = atoi(optarg);
+                        break;
+                case OPT_SPEEDUP_FRAME_SKIP:
+                        if (optarg)
+                            speedup_frame_skip = atoi(optarg);
+                        break;
 		}
 	}
 	return op;

src/common/ConfigManager.h
@@ -141,7 +141,9 @@ extern int winPauseNextFrame;
 extern uint32_t autoFrameSkipLastTime;
 extern uint32_t movieLastJoypad;
 extern uint32_t movieNextJoypad;
-extern int throttle;
+extern uint32_t throttle;
+extern uint32_t speedup_throttle;
+extern uint32_t speedup_frame_skip;
 
 extern int preparedCheats;
 extern const char *preparedCheatCodes[MAX_CHEATS];

src/wx/CMakeLists.txt
@@ -432,6 +432,7 @@ SET(XRC_SOURCES
     ${CMAKE_CURRENT_SOURCE_DIR}/xrc/PaletteViewer.xrc
     ${CMAKE_CURRENT_SOURCE_DIR}/xrc/SoundConfig.xrc
     ${CMAKE_CURRENT_SOURCE_DIR}/xrc/TileViewer.xrc
+    ${CMAKE_CURRENT_SOURCE_DIR}/xrc/SpeedupConfig.xrc
 )
 
 # wxrc does not support xrs files in -c output (> 10x compression)

src/wx/cmdevents.cpp
@@ -2041,6 +2041,14 @@ EVT_HANDLER(GeneralConfigure, "General options...")
     }
 }
 
+EVT_HANDLER(SpeedupConfigure, "Speedup / Turbo options...")
+{
+    wxDialog* dlg = GetXRCDialog("SpeedupConfig");
+
+    if (ShowModal(dlg) == wxID_OK)
+        update_opts();
+}
+
 EVT_HANDLER(GameBoyConfigure, "Game Boy options...")
 {
     wxDialog* dlg = GetXRCDialog("GameBoyConfig");

src/wx/guiinit.cpp
@@ -17,6 +17,7 @@
 #include <wx/filepicker.h>
 #include <wx/progdlg.h>
 #include <wx/spinctrl.h>
+#include <wx/valnum.h>
 #include <wx/stockitem.h>
 #include <wx/tokenzr.h>
 #include <wx/txtstrm.h>
@@ -2221,70 +2222,23 @@ public:
         DoSetThrottleSel(thr->GetValue());
     }
 
-    void DoSetThrottleSel(int val)
+    void DoSetThrottleSel(uint32_t val)
     {
-        switch (val) {
-        case 0:
-            thrsel->SetSelection(1);
-            break;
-
-        case 25:
-            thrsel->SetSelection(2);
-            break;
-
-        case 50:
-            thrsel->SetSelection(3);
-            break;
-
-        case 100:
-            thrsel->SetSelection(4);
-            break;
-
-        case 150:
-            thrsel->SetSelection(5);
-            break;
-
-        case 200:
-            thrsel->SetSelection(6);
-            break;
-
-        default:
-            thrsel->SetSelection(0);
-            break;
-        }
+        if (val <= 600)
+            thrsel->SetSelection(std::round((double)val / 25));
+        else
+            thrsel->SetSelection(100 / 25);
     }
 
     // set thr from thrsel
     void SetThrottle(wxCommandEvent& evt)
     {
-        switch (thrsel->GetSelection()) {
-        case 0: // blank; leave it alone
-            break;
+        uint32_t val = thrsel->GetSelection() * 25;
 
-        case 1:
-            thr->SetValue(0);
-            break;
-
-        case 2:
-            thr->SetValue(25);
-            break;
-
-        case 3:
-            thr->SetValue(50);
-            break;
-
-        case 4:
+        if (val <= 600)
+            thr->SetValue(val);
+        else
             thr->SetValue(100);
-            break;
-
-        case 5:
-            thr->SetValue(150);
-            break;
-
-        case 6:
-            thr->SetValue(200);
-            break;
-        }
     }
 
     // since this is not the actual dialog, derived from wxDialog, which is
@@ -2297,6 +2251,102 @@ public:
         DoSetThrottleSel(throttle);
     }
 } throttle_ctrl;
+
+// manage speedup key frame skip spinctrl/canned setting choice interaction
+static class SpeedupFrameSkipCtrl_t : public wxEvtHandler {
+public:
+    wxSpinCtrl* speedup_frame_skip_spin;
+    wxChoice* speedup_frame_skip_sel;
+    void SetSpeedupFrameSkipSel(wxSpinEvent& evt);
+    void DoSetSpeedupFrameSkipSel(uint32_t val);
+    void SetSpeedupFrameSkip(wxCommandEvent& evt);
+    void Init(wxShowEvent& ev);
+} speedup_frame_skip_ctrl;
+
+// manage speedup key throttle spinctrl/canned setting choice interaction
+static class SpeedupThrottleCtrl_t : public wxEvtHandler {
+public:
+    wxSpinCtrl* speedup_throttle_spin;
+    wxChoice* speedup_throttle_sel;
+
+    // set speedup_throttle_sel from speedup_throttle
+    void SetSpeedupThrottleSel(wxSpinEvent& evt)
+    {
+        DoSetSpeedupThrottleSel(speedup_throttle_spin->GetValue());
+    }
+
+    void DoSetSpeedupThrottleSel(uint32_t val)
+    {
+        if (val > 0 && val <= 600) {
+            speedup_throttle_sel->SetSelection(std::round((double)val / 25));
+            speedup_frame_skip_ctrl.DoSetSpeedupFrameSkipSel(0);
+            wxCommandEvent nil;
+            speedup_frame_skip_ctrl.SetSpeedupFrameSkip(nil);
+        }
+        else
+            speedup_throttle_sel->SetSelection(0);
+    }
+
+    // set speedup_throttle from speedup_throttle_sel
+    void SetSpeedupThrottle(wxCommandEvent& evt)
+    {
+        uint32_t val = speedup_throttle_sel->GetSelection() * 25;
+
+        if (val > 0 && val <= 600) {
+            speedup_throttle_spin->SetValue(val);
+            speedup_frame_skip_ctrl.DoSetSpeedupFrameSkipSel(0);
+            wxCommandEvent nil;
+            speedup_frame_skip_ctrl.SetSpeedupFrameSkip(nil);
+        }
+        else
+            speedup_throttle_spin->SetValue(0);
+    }
+
+    void Init(wxShowEvent& ev)
+    {
+        ev.Skip();
+        DoSetSpeedupThrottleSel(speedup_throttle);
+    }
+} speedup_throttle_ctrl;
+
+// set speedup_frame_skip_sel from speedup_frame_skip
+void SpeedupFrameSkipCtrl_t::SetSpeedupFrameSkipSel(wxSpinEvent& evt)
+{
+    DoSetSpeedupFrameSkipSel(speedup_frame_skip_spin->GetValue());
+}
+
+void SpeedupFrameSkipCtrl_t::DoSetSpeedupFrameSkipSel(uint32_t val)
+{
+    if (val > 0 && val <= 30) {
+        speedup_frame_skip_sel->SetSelection(val);
+        speedup_throttle_ctrl.DoSetSpeedupThrottleSel(0);
+        wxCommandEvent nil;
+        speedup_throttle_ctrl.SetSpeedupThrottle(nil);
+    }
+    else
+        speedup_frame_skip_sel->SetSelection(0);
+}
+
+// set speedup_frame_skip from speedup_frame_skip_sel
+void SpeedupFrameSkipCtrl_t::SetSpeedupFrameSkip(wxCommandEvent& evt)
+{
+    uint32_t val = speedup_frame_skip_sel->GetSelection();
+
+    if (val > 0 && val <= 30) {
+        speedup_frame_skip_spin->SetValue(val);
+        speedup_throttle_ctrl.DoSetSpeedupThrottleSel(0);
+        wxCommandEvent nil;
+        speedup_throttle_ctrl.SetSpeedupThrottle(nil);
+    }
+    else
+        speedup_frame_skip_spin->SetValue(0);
+}
+
+void SpeedupFrameSkipCtrl_t::Init(wxShowEvent& ev)
+{
+    ev.Skip();
+    DoSetSpeedupFrameSkipSel(speedup_frame_skip);
+}
 
 /////////////////////////////
 //Check if a pointer from the XRC file is valid. If it's not, throw an error telling the user.
@@ -3207,6 +3257,11 @@ bool MainFrame::BindControls()
         sc = SafeXRCCTRL<wxSpinCtrl>(d, n);       \
         sc->SetValidator(wxGenericValidator(&o)); \
     } while (0)
+#define getsc_uint(n, o)                               \
+    do {                                          \
+        sc = SafeXRCCTRL<wxSpinCtrl>(d, n);       \
+        sc->SetValidator(wxUIntValidator(&o)); \
+    } while (0)
         {
             // Online Auto Update check frequency
             getrbi("UpdateNever", gopts.onlineupdates, 0);
@@ -3215,7 +3270,7 @@ bool MainFrame::BindControls()
             getrbi("PNG", captureFormat, 0);
             getrbi("BMP", captureFormat, 1);
             getsc("RewindInterval", gopts.rewind_interval);
-            getsc("Throttle", throttle);
+            getsc_uint("Throttle", throttle);
             throttle_ctrl.thr = sc;
             throttle_ctrl.thrsel = SafeXRCCTRL<wxChoice>(d, "ThrottleSel");
             throttle_ctrl.thr->Connect(wxEVT_COMMAND_SPINCTRL_UPDATED,
@@ -3226,6 +3281,35 @@ bool MainFrame::BindControls()
                 NULL, &throttle_ctrl);
             d->Connect(wxEVT_SHOW, wxShowEventHandler(ThrottleCtrl_t::Init),
                 NULL, &throttle_ctrl);
+            d->Fit();
+        }
+        // SpeedUp Key Config
+        d = LoadXRCDialog("SpeedupConfig");
+        {
+            getsc_uint("SpeedupThrottle", speedup_throttle);
+            speedup_throttle_ctrl.speedup_throttle_spin = sc;
+            speedup_throttle_ctrl.speedup_throttle_sel = SafeXRCCTRL<wxChoice>(d, "SpeedupThrottleSel");
+            speedup_throttle_ctrl.speedup_throttle_spin->Connect(wxEVT_COMMAND_SPINCTRL_UPDATED,
+                wxSpinEventHandler(SpeedupThrottleCtrl_t::SetSpeedupThrottleSel),
+                NULL, &speedup_throttle_ctrl);
+            speedup_throttle_ctrl.speedup_throttle_sel->Connect(wxEVT_COMMAND_CHOICE_SELECTED,
+                wxCommandEventHandler(SpeedupThrottleCtrl_t::SetSpeedupThrottle),
+                NULL, &speedup_throttle_ctrl);
+            d->Connect(wxEVT_SHOW, wxShowEventHandler(SpeedupThrottleCtrl_t::Init),
+                NULL, &speedup_throttle_ctrl);
+            d->Fit();
+
+            getsc_uint("SpeedupFrameSkip", speedup_frame_skip);
+            speedup_frame_skip_ctrl.speedup_frame_skip_spin = sc;
+            speedup_frame_skip_ctrl.speedup_frame_skip_sel = SafeXRCCTRL<wxChoice>(d, "SpeedupFrameSkipSel");
+            speedup_frame_skip_ctrl.speedup_frame_skip_spin->Connect(wxEVT_COMMAND_SPINCTRL_UPDATED,
+                wxSpinEventHandler(SpeedupFrameSkipCtrl_t::SetSpeedupFrameSkipSel),
+                NULL, &speedup_frame_skip_ctrl);
+            speedup_frame_skip_ctrl.speedup_frame_skip_sel->Connect(wxEVT_COMMAND_CHOICE_SELECTED,
+                wxCommandEventHandler(SpeedupFrameSkipCtrl_t::SetSpeedupFrameSkip),
+                NULL, &speedup_frame_skip_ctrl);
+            d->Connect(wxEVT_SHOW, wxShowEventHandler(SpeedupFrameSkipCtrl_t::Init),
+                NULL, &speedup_frame_skip_ctrl);
             d->Fit();
         }
 #define getcbbe(n, o) getbe(n, o, cb, wxCheckBox, CB)

src/wx/opts.cpp
@@ -26,6 +26,10 @@
     {                                                        \
         wxT(c), (n), d, NULL, NULL, wxT(""), min, max, NULL, &v \
     }
+#define UINTOPT(c, n, d, v, min, max)                      \
+    {                                                        \
+        wxT(c), (n), d, NULL, NULL, wxT(""), min, max, NULL, NULL, &v \
+    }
 #define BOOLOPT(c, n, d, v)                        \
     {                                              \
         wxT(c), (n), d, NULL, NULL, wxT(""), 0, 0, &v \
@@ -252,7 +256,9 @@ opt_desc opts[] = {
     INTOPT("preferences/skipBios", "SkipIntro", wxTRANSLATE("Skip BIOS initialization"), skipBios, 0, 1),
     INTOPT("preferences/skipSaveGameCheats", "", wxTRANSLATE("Do not overwrite cheat list when loading state"), skipSaveGameCheats, 0, 1),
     INTOPT("preferences/skipSaveGameBattery", "", wxTRANSLATE("Do not overwrite native (battery) save when loading state"), skipSaveGameBattery, 0, 1),
-    INTOPT("preferences/throttle", "", wxTRANSLATE("Throttle game speed, even when accelerated (0-1000%, 0 = disabled)"), throttle, 0, 1000),
+    UINTOPT("preferences/throttle", "", wxTRANSLATE("Throttle game speed, even when accelerated (0-500%, 0 = no throttle)"), throttle, 0, 600),
+    UINTOPT("preferences/speedupThrottle", "", wxTRANSLATE("Set throttle for speedup key (0-600%, 0 = no throttle)"), speedup_throttle, 0, 600),
+    UINTOPT("preferences/speedupFrameSkip", "", wxTRANSLATE("Set frame skip for speedup key (0-30)"), speedup_frame_skip, 0, 30),
     INTOPT("preferences/useBiosGB", "BootRomGB", wxTRANSLATE("Use the specified BIOS file for GB"), useBiosFileGB, 0, 1),
     INTOPT("preferences/useBiosGBA", "BootRomEn", wxTRANSLATE("Use the specified BIOS file"), useBiosFileGBA, 0, 1),
     INTOPT("preferences/useBiosGBC", "BootRomGBC", wxTRANSLATE("Use the specified BIOS file for GBC"), useBiosFileGBC, 0, 1),
@@ -525,6 +531,15 @@ void load_opts()
                 wxLogWarning(_("Invalid value %f for option %s; valid values are %f - %f"), opt.curdouble, opt.opt, opt.min, opt.max);
             } else
                 *opt.doubleopt = opt.curdouble;
+        } else if (opt.uintopt) {
+            int val;
+            cfg->Read(opt.opt, &val, *opt.uintopt);
+            opt.curuint = val;
+
+            if (opt.curuint < opt.min || opt.curuint > opt.max) {
+                wxLogWarning(_("Invalid value %f for option %s; valid values are %f - %f"), opt.curuint, opt.opt, opt.min, opt.max);
+            } else
+                *opt.uintopt = opt.curuint;
         } else if (opt.boolopt) {
             cfg->Read(opt.opt, opt.boolopt, *opt.boolopt);
             opt.curbool = *opt.boolopt;
@@ -648,6 +663,9 @@ void update_opts()
         } else if (opt.doubleopt) {
             if (*opt.doubleopt != opt.curdouble)
                 cfg->Write(opt.opt, (opt.curdouble = *opt.doubleopt));
+        } else if (opt.uintopt) {
+            if (*opt.uintopt != opt.curuint)
+                cfg->Write(opt.opt, (opt.curuint = *opt.uintopt));
         } else if (opt.boolopt) {
             if (*opt.boolopt != opt.curbool)
                 cfg->Write(opt.opt, (opt.curbool = *opt.boolopt));
@@ -802,6 +820,14 @@ bool opt_set(const wxString& name, const wxString& val)
                 wxLogWarning(_("Invalid value %f for option %s; valid values are %f - %f"), dval, name, opt->min, opt->max);
             else
                 *opt->doubleopt = dval;
+        } else if (opt->uintopt) {
+            const wxString s(val);
+            unsigned long uival;
+
+            if (!s.ToULong(&uival) || uival < opt->min || uival > opt->max)
+                wxLogWarning(_("Invalid value %f for option %s; valid values are %f - %f"), uival, name, opt->min, opt->max);
+            else
+                *opt->uintopt = (uint32_t)uival;
         } else {
             // GB/Palette[0-2] is virtual
             for (int i = 0; i < 3; i++) {

commit eb6dfb4bfaaad1b70b63d982a67bed2229f129b5

fix libretro build broken in 16dd5d40 #339
Conditionally compile out the code for the feature implemented in
16dd5d40 (which is the throttle and frame skip configuration for the
speedup button) for libretro, and use the old behavior of skipping 9
frames.

Affects GBA.cpp and GB.cpp .

Signed-off-by: Rafael Kitover <rkitover@gmail.com>

src/gb/GB.cpp
@@ -4843,6 +4843,8 @@ void gbEmulate(int ticksToStop)
 
                 if ((gbLcdTicksDelayed <= 0) && (gbLCDChangeHappened)) {
                     int framesToSkip = systemFrameSkip;
+
+#ifndef __LIBRETRO__
                     static bool speedup_throttle_set = false;
                     static uint32_t last_throttle;
 
@@ -4868,6 +4870,10 @@ void gbEmulate(int ticksToStop)
 
                         speedup_throttle_set = false;
                     }
+#else
+                    if ((gbJoymask[0] >> 10) & 1)
+                        framesToSkip = 9;
+#endif
 
                     //gbLcdTicksDelayed = gbLcdTicks+1;
                     gbLCDChangeHappened = false;
@@ -4946,7 +4952,11 @@ void gbEmulate(int ticksToStop)
 
                             speedup = false;
 
+#ifndef __LIBRETRO__
                             if (newmask & 1 && speedup_throttle == 0)
+#else
+                            if (newmask & 1)
+#endif
                                 speedup = true;
 
                             gbCapture = (newmask & 2) ? true : false;

src/gba/GBA.cpp
@@ -3756,6 +3756,8 @@ void CPULoop(int ticks)
                     }
                 } else {
                     int framesToSkip = systemFrameSkip;
+
+#ifndef __LIBRETRO__
                     static bool speedup_throttle_set = false;
                     static uint32_t last_throttle;
 
@@ -3781,6 +3783,10 @@ void CPULoop(int ticks)
 
                         speedup_throttle_set = false;
                     }
+#else
+                    if ((joy >> 10) & 1)
+                        framesToSkip = 9;
+#endif
 
                     if (DISPSTAT & 2) {
                         // if in H-Blank, leave it and move to drawing mode
@@ -3814,7 +3820,11 @@ void CPULoop(int ticks)
 
                             speedup = false;
 
+#ifndef __LIBRETRO__
                             if (ext & 1 && speedup_throttle == 0)
+#else
+                            if (ext & 1)
+#endif
                                 speedup = true;
 
                             capture = (ext & 2) ? true : false;