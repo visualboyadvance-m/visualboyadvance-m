d9a7df6105967a60d153c8738226a7c5969ef27a
Rafael Kitover <rkitover@gmail.com>
2019-04-01 08:42:52 -0700

    fix if statement in SDL sound driver #396
    @denisfa found this due to a warning, this should improve the
    functionality of the SDL sound driver.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- src/common/SoundSDL.cpp
+++ src/common/SoundSDL.cpp
@@ -60,11 +60,12 @@ void SoundSDL::read(uint16_t* stream, int length) {
     if (!initialized || !emulating)
         return;
 
-    if (!buffer_size())
+    if (!buffer_size()) {
         if (should_wait())
             SDL_SemWait(data_available);
         else
             return;
+    }
 
     SDL_LockMutex(mutex);
 
