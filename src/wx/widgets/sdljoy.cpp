visualboyadvance-m/visualboyadvance-m

commit e57beed8906638a1309076be8f453f299cefddca

ignore depressed gamepad triggers #88
On joystick event notification, filter out axis values that are equal to
their initial state.

The reason for doing this is explained in 539027ca. Triggers on e.g. the
360 controller are an axis that are the max negative value in the
depressed state, and for some reason a constant stream of events are
generated for them, that's another issue that needs to be addressed.

For the time being, this fixes the other half of the main issue in #88:
users with an old config with the default special key bindings will now
be able to use keyboard hotkeys as these spurious joystick events will
be filtered out and will not block keyboard events.

TODO:

- stop generating events for depressed triggers in the first place

- fix joystick events completely blocking keyboard events

src/wx/widgets/sdljoy.cpp
@@ -134,6 +134,11 @@ void wxSDLJoy::Notify()
             for (int j = 0; j < nax; j++) {
                 val = SDL_JoystickGetAxis(dev, j);
 
+                // trigger axes always return max negative value, we ignore these
+                int16_t initial_state;
+                if (SDL_JoystickGetAxisInitialState(dev, j, &initial_state) && val == initial_state)
+                    continue;
+
                 if (digital) {
                     if (val > 0x3fff)
                         val = 0x7fff;

commit 64a9c0945f870d150d194865e8b9d342b2630b6f

fix analog stick regression from e57beed8 #400
Instead of ignoring the initial state of axes, which I did to make
triggers work on the 360 controller, set the initial previous value to
the initial state instead.

This fixes the original problem without breaking analog stick movement.

Signed-off-by: Rafael Kitover <rkitover@gmail.com>

src/wx/widgets/sdljoy.cpp
@@ -36,7 +36,7 @@ wxSDLJoy::wxSDLJoy(bool analog)
     if (!njoy)
         return;
 
-    joystate = new wxSDLJoyState_t[njoy];
+    joystate = new wxSDLJoyState[njoy];
     memset(joystate, 0, njoy * sizeof(*joystate));
 
     for (int i = 0; i < njoy; i++) {
@@ -45,7 +45,17 @@ wxSDLJoy::wxSDLJoy(bool analog)
         nctrl += joystate[i].nax = SDL_JoystickNumAxes(dev);
         nctrl += joystate[i].nhat = SDL_JoystickNumHats(dev);
         nctrl += joystate[i].nbut = SDL_JoystickNumButtons(dev);
-        joystate[i].curval = new short[nctrl];
+        joystate[i].curval = new short[nctrl]{0};
+
+        // initialize axis previous value to initial state
+#if SDL_VERSION_ATLEAST(2, 0, 6)
+        for (int j = 0; j < joystate[i].nax; j++) {
+            int16_t initial_state = 0;
+            SDL_JoystickGetAxisInitialState(dev, j, &initial_state);
+            joystate[i].curval[j] = initial_state;
+        }
+#endif
+
         memset(joystate[i].curval, 0, sizeof(short) * nctrl);
     }
 }
@@ -133,13 +143,6 @@ void wxSDLJoy::Notify()
 
             for (int j = 0; j < nax; j++) {
                 val = SDL_JoystickGetAxis(dev, j);
-
-                // trigger axes always return max negative value, we ignore these
-#if SDL_VERSION_ATLEAST(2, 0, 6)
-                int16_t initial_state;
-                if (SDL_JoystickGetAxisInitialState(dev, j, &initial_state) && val == initial_state)
-                    continue;
-#endif
 
                 if (digital) {
                     if (val > 0x3fff)

src/wx/widgets/wx/sdljoy.h
@@ -13,7 +13,7 @@
 #include <wx/event.h>
 #include <wx/timer.h>
 
-typedef struct wxSDLJoyState wxSDLJoyState_t;
+struct wxSDLJoyState;
 
 class wxSDLJoy : public wxTimer {
 public:
@@ -61,7 +61,7 @@ public:
 protected:
     bool digital;
     int njoy;
-    wxSDLJoyState_t* joystate;
+    wxSDLJoyState* joystate;
     wxEvtHandler* evthandler;
     bool nosticks;
     void Notify();