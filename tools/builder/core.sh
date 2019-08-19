visualboyadvance-m/visualboyadvance-m

commit e2f06210e07b3eb5770df7a56a1330478ea20035

update Visual Studio support
Replace the vcpkg code with a newer and better version. In this version
the `vcpkg` directory is on the same level as the source directory, so
that IntelliSense does not get confused scanning for files.

The cmake variable `VCPKG_TARGET_TRIPLET` is required to activate vcpkg
support, it must be set to e.g. `x64-windows` or `x86-windows`.

Update `CMakeSettings.json` for the new code for the GUI.

Fix a problem copying the `SDL2.dll` file to the binary directory.

Update `README.md` with commandline instructions.

Signed-off-by: Rafael Kitover <rkitover@gmail.com>

CMakeLists.txt
@@ -11,14 +11,11 @@ if(COMMAND cmake_policy)
     endif()
 endif()
 
-option(ENABLE_VCPKG "Use dependencies for Visual Studio from vcpkg" ON)
+set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake)
 
-set(NLS_DEFAULT ON)
-
-# get win32 deps before project declaration, because toolchain is set for vcpkg
 set(VCPKG_DEPS zlib libpng SDL2 SFML gettext wxWidgets)
 
-include(${CMAKE_SOURCE_DIR}/cmake/Win32Deps.cmake)
+include(Set-Toolchain-vcpkg)
 
 project(VBA-M C CXX)
 
@@ -34,7 +31,6 @@ endif()
 
 set(ALL_TARGETS fex visualboyadvance-m vbamcore vbam)
 
-set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake)
 #Output all binaries at top level
 set(EXECUTABLE_OUTPUT_PATH ${PROJECT_BINARY_DIR})
 
@@ -124,7 +120,28 @@ endif()
 
 option(ENABLE_LTO "Compile with Link Time Optimization (gcc and clang only)" ${LTO_DEFAULT})
 
+if(WIN32 OR CMAKE_TOOLCHAIN_FILE MATCHES "[Mm][Ii][Nn][Gg][Ww]")
+    # Win32 deps submodule
+    if(NOT EXISTS "${CMAKE_SOURCE_DIR}/dependencies/mingw-xaudio/include")
+        set(git_checkout FALSE)
+
+        if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
+            set(git_checkout TRUE)
+            execute_process(
+                COMMAND git submodule update --init --remote --recursive
+                RESULT_VARIABLE git_status
+                WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+            )
+        endif()
+
+        if(NOT (git_checkout AND git_status EQUAL 0))
+            message(FATAL_ERROR "Please pull in git submodules, e.g.\nrun: git submodule update --init --remote --recursive")
+        endif()
+    endif()
+endif()
+
 if(MSVC)
+    include_directories("${CMAKE_SOURCE_DIR}/dependencies/msvc")
     add_definitions(-D_CRT_SECURE_NO_WARNINGS)
 endif()
 
@@ -938,5 +955,4 @@ set(CPACK_PACKAGE_VERSION_MAJOR "2")
 set(CPACK_PACKAGE_VERSION_MINOR "0")
 set(CPACK_PACKAGE_VERSION_PATCH "0-Git-" ${COMMITHASH})
 list(APPEND CPACK_SOURCE_IGNORE_FILES "${CMAKE_CURRENT_SOURCE_DIR}/dependencies")
-list(APPEND CPACK_SOURCE_IGNORE_FILES "${CMAKE_CURRENT_SOURCE_DIR}/vcpkg")
 include(CPack)

src/wx/CMakeLists.txt
@@ -119,7 +119,7 @@ if(WIN32 AND CMAKE_TOOLCHAIN_FILE MATCHES vcpkg AND (X86_32 OR AMD64))
 
     # SDL2.dll does not get copied to build dir
     if(NOT EXISTS ${CMAKE_BINARY_DIR}/SDL2.dll)
-        file(COPY ${_VCPKG_ROOT_DIR}/installed/${WINARCH}-windows${suffix}/${path_prefix}/bin/SDL2.dll DESTINATION ${CMAKE_BINARY_DIR})
+        file(COPY ${_VCPKG_ROOT_DIR}/installed/${WINARCH}-windows/${path_prefix}/bin/SDL2.dll DESTINATION ${CMAKE_BINARY_DIR})
     endif()
 else()
     if(CMAKE_BUILD_TYPE MATCHES Debug)

.gitmodules
@@ -1,6 +1,3 @@
 [submodule "dependencies"]
 	path = dependencies
 	url = https://github.com/visualboyadvance-m/dependencies.git
-[submodule "vcpkg"]
-	path = vcpkg
-	url = https://github.com/Microsoft/vcpkg.git

cmake/Architecture.cmake
@@ -18,7 +18,9 @@ if(CMAKE_SYSTEM_PROCESSOR MATCHES "[xX]86|i[3-9]86|[aA][mM][dD]64")
             set(ASM_DEFAULT ON)
         endif()
         set(X86_32 ON)
+	set(WINARCH x86)
     else()
         set(AMD64 ON)
+	set(WINARCH x64)
     endif()
 endif()

cmake/Set-Toolchain-vcpkg.cmake
@@ -1,0 +1,63 @@
+if(VCPKG_TARGET_TRIPLET)
+    if(NOT DEFINED ENV{VCPKG_ROOT})
+        get_filename_component(VCPKG_ROOT ${CMAKE_SOURCE_DIR}/../vcpkg ABSOLUTE)
+        set(ENV{VCPKG_ROOT} ${VCPKG_ROOT})
+    else()
+        set(VCPKG_ROOT $ENV{VCPKG_ROOT})
+    endif()
+
+    if(NOT EXISTS ${VCPKG_ROOT})
+        get_filename_component(vcpkg_root_parent ${VCPKG_ROOT}/.. ABSOLUTE)
+
+        execute_process(
+            COMMAND git clone https://github.com/microsoft/vcpkg.git
+            RESULT_VARIABLE git_status
+            WORKING_DIRECTORY ${vcpkg_root_parent}
+        )
+
+        if(NOT git_status EQUAL 0)
+            message(FATAL_ERROR "Error cloning vcpkg, please make sure git for windows is installed correctly, it can be installed from Visual Studio components")
+        endif()
+    endif()
+
+    # build vcpkg if not built
+    if(WIN32)
+        if(NOT EXISTS ${VCPKG_ROOT}/vcpkg.exe)
+            execute_process(
+                COMMAND bootstrap-vcpkg.bat
+                WORKING_DIRECTORY ${VCPKG_ROOT}
+            )
+        endif()
+    else()
+        if(NOT EXISTS ${VCPKG_ROOT}/vcpkg)
+            execute_process(
+                COMMAND ./bootstrap-vcpkg.sh
+                WORKING_DIRECTORY ${VCPKG_ROOT}
+            )
+        endif()
+    endif()
+
+    foreach(pkg ${VCPKG_DEPS})
+        list(APPEND VCPKG_DEPS_QUALIFIED ${pkg}:${VCPKG_TARGET_TRIPLET})
+    endforeach()
+
+    # build our deps
+    if(WIN32)
+        execute_process(
+            COMMAND vcpkg install ${VCPKG_DEPS_QUALIFIED}
+            WORKING_DIRECTORY ${VCPKG_ROOT}
+        )
+    else()
+        execute_process(
+            COMMAND ./vcpkg install ${VCPKG_DEPS_QUALIFIED}
+            WORKING_DIRECTORY ${VCPKG_ROOT}
+        )
+    endif()
+
+    if(WIN32 AND VCPKG_TARGET_TRIPLET MATCHES x64)
+        set(CMAKE_GENERATOR_PLATFORM x64 CACHE STRING "visual studio build architecture" FORCE)
+    endif()
+
+    set(CMAKE_TOOLCHAIN_FILE ${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake CACHE FILEPATH "vcpkg toolchain" FORCE)
+    include(${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake)
+endif()

cmake/Win32Deps.cmake
@@ -1,65 +1,0 @@
-if(WIN32 OR CMAKE_TOOLCHAIN_FILE MATCHES "[Mm][Ii][Nn][Gg][Ww]")
-    # compiler has not been detected yet maybe
-    if(CMAKE_C_COMPILER MATCHES "cl\\.exe" OR CMAKE_CXX_COMPILER MATCHES "cl\\.exe" OR MSVC OR DEFINED ENV{VisualStudioVersion})
-        set(VS TRUE)
-    endif()
-
-    set(WINARCH x86)
-    if(CMAKE_C_COMPILER MATCHES x64 OR CMAKE_CXX_COMPILER MATCHES x64 OR "$ENV{VSCMD_ARG_TGT_ARCH}" MATCHES x64)
-        set(WINARCH x64)
-    endif()
-
-    # Win32 deps submodules (dependencies and vcpkg)
-    if(NOT EXISTS "${CMAKE_SOURCE_DIR}/dependencies/mingw-xaudio/include" OR NOT EXISTS "${CMAKE_SOURCE_DIR}/vcpkg/scripts/buildsystems/vcpkg.cmake")
-        set(git_checkout FALSE)
-
-        if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
-            set(git_checkout TRUE)
-            execute_process(
-                COMMAND git submodule update --init --remote --recursive
-                RESULT_VARIABLE git_status
-                WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
-            )
-        endif()
-
-        if(NOT (git_checkout AND git_status EQUAL 0))
-            message(FATAL_ERROR "Please pull in git submodules, e.g.\nrun: git submodule update --init --remote --recursive")
-        endif()
-    endif()
-
-    if(VS)
-        set(DEPS_MSVC "${CMAKE_SOURCE_DIR}/dependencies/msvc")
-        include_directories("${DEPS_MSVC}") # for GL/glext.h and getopt.h
-    endif()
-
-    if(VS AND ENABLE_VCPKG)
-        if(NOT DEFINED ENV{VCPKG_ROOT})
-            set(ENV{VCPKG_ROOT} "${CMAKE_SOURCE_DIR}/vcpkg")
-        endif()
-
-        # build vcpkg if not built
-        if(NOT EXISTS $ENV{VCPKG_ROOT}/vcpkg.exe)
-            execute_process(
-                COMMAND bootstrap-vcpkg.bat
-                WORKING_DIRECTORY $ENV{VCPKG_ROOT}
-            )
-        endif()
-
-        foreach(pkg ${VCPKG_DEPS})
-            #list(APPEND VCPKG_DEPS_QUALIFIED ${pkg}:${WINARCH}-windows-static)
-            list(APPEND VCPKG_DEPS_QUALIFIED ${pkg}:${WINARCH}-windows)
-        endforeach()
-
-        # build our deps
-        execute_process(
-            COMMAND vcpkg install ${VCPKG_DEPS_QUALIFIED}
-            WORKING_DIRECTORY $ENV{VCPKG_ROOT}
-        )
-
-        set(CMAKE_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE FILEPATH '' FORCE)
-        include("$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
-
-        set(NLS_DEFAULT OFF)
-        set(ENABLE_NLS OFF) # not sure why this is necessary
-    endif()
-endif()

commit 2958af01caff6c5ce8077b166749f8592092c35d

builder: mingw improvements + misc.
- default ENABLE_NLS to ON except for vcpkg, broken in e2f06210

- update zlib URL

- use openssl 1.1.1c instead of 1.0.2p, parallel build works now!

- add osslsigncode for mingw to codesign windows binaries, and
  automatically sign in `build_project()`

- add curl (for osslsigncode, which needs libcurl)

- use current master for wxwidgets instead of 3.1.2

- make the `translations.zip` in `build_project()` for windows

- on mac, unlock the login keychain before codesigning (this is
necessary if using e.g. ssh.)

- do not fail if the user does not have codesigning or gpg keys

- fix wxwidgets configure options on mac (need to append to
  `DIST_CONFIGURE_OVERRIDES` instead of `DIST_ARGS`.)

Signed-off-by: Rafael Kitover <rkitover@gmail.com>

CMakeLists.txt
@@ -357,6 +357,12 @@ if(NOT ENABLE_ASM_CORE)
     add_definitions(-DC_CORE)
 endif()
 
+set(NLS_DEFAULT OFF)
+
+if(NOT CMAKE_TOOLCHAIN_FILE MATCHES vcpkg)
+    set(NLS_DEFAULT ON)
+endif()
+
 option(ENABLE_NLS "Enable translations" ${NLS_DEFAULT})
 
 # Enable internationalization

tools/builder/core.sh
@@ -105,10 +105,11 @@ DISTS=$DISTS'
     bzip2           https://github.com/nemequ/bzip2/releases/download/v1.0.6/bzip2-1.0.6.tar.gz                 lib/libbz2.a
     xz              https://tukaani.org/xz/xz-5.2.3.tar.gz                                                      lib/liblzma.a
     unzip           https://downloads.sourceforge.net/project/infozip/UnZip%206.x%20%28latest%29/UnZip%206.0/unzip60.tar.gz     bin/unzip
-    zlib            https://zlib.net/zlib-1.2.11.tar.gz                                                         lib/libz.a
+    zlib            https://www.zlib.net/zlib-1.2.11.tar.gz                                                     lib/libz.a
     ccache          https://www.samba.org/ftp/ccache/ccache-3.4.3.tar.xz                                        bin/ccache
     zip             https://downloads.sourceforge.net/project/infozip/Zip%203.x%20%28latest%29/3.0/zip30.tar.gz                 bin/zip
-    openssl         https://www.openssl.org/source/openssl-1.0.2p.tar.gz                                        lib/libssl.a
+    openssl         https://www.openssl.org/source/openssl-1.1.1c.tar.gz                                        lib/libssl.a
+    curl            https://curl.haxx.se/download/curl-7.65.3.tar.xz                                            lib/libcurl.a
     cmake           https://cmake.org/files/v3.13/cmake-3.13.0-rc1.tar.gz                                       bin/cmake
     m4              http://ftp.gnu.org/gnu/m4/m4-1.4.18.tar.xz                                                  bin/m4
     autoconf        https://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.xz                                       bin/autoconf
@@ -182,7 +183,7 @@ DISTS=$DISTS'
     harfbuzz        https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-1.7.5.tar.bz2                lib/libharfbuzz.a
     sfml            https://github.com/SFML/SFML/archive/013d053277c980946bc7761a2a088f1cbb788f8c.tar.gz        lib/libsfml-system-s.a
     shared-mime-info http://freedesktop.org/~hadess/shared-mime-info-1.9.tar.xz                                 bin/update-mime-database
-    wxwidgets       https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.2/wxWidgets-3.1.2.tar.bz2     lib/libwx_baseu-3.1*.a
+    wxwidgets       https://github.com/wxWidgets/wxWidgets/archive/bcca16911d917a39d86a183ec976d9f4759e56d0.tar.gz lib/libwx_baseu-3.1*.a
     graphite2       https://github.com/silnrsi/graphite/releases/download/1.3.10/graphite2-1.3.10.tgz           lib/libgraphite2.a
     xvidcore        http://downloads.xvid.org/downloads/xvidcore-1.3.4.tar.bz2                                  lib/libxvidcore.a
     fribidi         https://github.com/fribidi/fribidi/releases/download/v1.0.1/fribidi-1.0.1.tar.bz2           lib/libfribidi.a
@@ -269,7 +270,6 @@ DIST_RELOCATION_TYPES="$DIST_RELOCATION_TYPES
 
 DIST_PRE_BUILD="$DIST_PRE_BUILD
 #    xz              mkdir -p build-aux; touch build-aux/config.rpath; mkdir -p po; touch po/Makefile.in.in; sed -i.bak 's/ po / /g' Makefile.am;
-    openssl         sed -E -i.bak '/^build_all:/{; s/ build_tests / /; }' Makefile.org;
     getopt          sed -i.bak 's/\\\$(LDFLAGS)\\(.*\\)\$/\\1 \$(LDFLAGS)/' Makefile;
     libicu          cd source;
 #    flex-2.6.3      sed -i.bak '/^'\"\$TAB\"'tests \\\\\$/d' Makefile.am;
@@ -319,15 +319,6 @@ DIST_POST_BUILD="$DIST_POST_BUILD
                     sed -i.bak \"s|/usr/share/fonts|\$BUILD_ROOT/root/share/fonts|g\" \"\$BUILD_ROOT/root/etc/fonts/fonts.conf\";
 "
 
-DIST_POST_CONFIGURE="$DIST_POST_CONFIGURE
-    #
-    # I tried to use this to make openssl build in parallel, but it fails unpredictably.
-    #
-    #openssl         sed -E -i.bak ' \
-    #    s/^(\\t+)([^\\t]+\\\$\\((BUILD_ONE_CMD|RECURSIVE_BUILD_CMD|RECURSIVE_MAKE)\\))/\1+ \2/ \
-    #' \$(find . -name Makefile);
-"
-
 DIST_CONFIGURE_OVERRIDES="$DIST_CONFIGURE_OVERRIDES
     openssl     ./config no-shared --prefix=/usr --openssldir=/etc/ssl
     cmake       ./configure --prefix=/usr --no-qt-gui --parallel=\$NUM_CPUS --enable-ccache
@@ -364,6 +355,7 @@ DIST_ARGS="$DIST_ARGS
     libicu      --disable-extras --disable-tools --disable-tests --disable-samples
     gettext     --with-included-gettext --with-included-glib --with-included-libcroco --with-included-libunistring --with-included-libxml --disable-curses CPPFLAGS=\"\$CPPFLAGS -DLIBXML_STATIC\"
     pkgconfig   --with-internal-glib --with-libiconv=gnu
+    curl        --with-ssl
     pcre        --enable-utf8 --enable-pcre8 --enable-pcre16 --enable-pcre32 --enable-unicode-properties --enable-pcregrep-libz --enable-pcregrep-libbz2 --enable-jit
     libxslt     --without-python --without-crypto
     libgd       --without-xpm
@@ -405,9 +397,8 @@ export DIST_BARE_MAKE_ARGS='CC="$CC"'
 
 export ALL_MAKE_ARGS='V=1 VERBOSE=1'
 
-# have to disable ccache for openssl
 DIST_MAKE_ARGS="$DIST_MAKE_ARGS
-    openssl     CC=\"\$CC\" CXX=\"\$CXX\" LDFLAGS=\"\$LDFLAGS\" -j1
+    openssl     CC=\"\$CC\"
     getopt      LDFLAGS=\"\$LDFLAGS -lintl -liconv\" CFLAGS=\"\$CFLAGS\"
     bzip2       libbz2.a bzip2 bzip2recover CFLAGS=\"\$CFLAGS\" LDFLAGS=\"\$LDFLAGS\"
     unzip       generic2
@@ -2626,12 +2617,17 @@ build_project() {
     mkdir -p "$BUILD_ROOT/project"
     cd "$BUILD_ROOT/project"
 
+    rm -f visualboyadvance-m.exe
+
     # FIXME: turn LTO back on when everything works
-    echo_eval_run cmake "'$CHECKOUT'" $REQUIRED_CMAKE_ARGS -DVBAM_STATIC=ON -DENABLE_LTO=OFF $CMAKE_BASE_ARGS $PROJECT_ARGS $@
+    echo_eval_run cmake "'$CHECKOUT'" $REQUIRED_CMAKE_ARGS -DVBAM_STATIC=ON -DENABLE_LTO=ON $CMAKE_BASE_ARGS $PROJECT_ARGS $@
     echo_run make -j$NUM_CPUS VERBOSE=1
 
     if [ "$target_os" = mac ]; then
         $STRIP visualboyadvance-m.app/Contents/MacOS/visualboyadvance-m
+
+        # unlock keychain for codesigning certificate
+        security -v unlock-keychain ~/Library/Keychains/login.keychain* || :
 
         codesign -s "Developer ID Application" --deep ./visualboyadvance-m.app || :
 
@@ -2640,18 +2636,35 @@ build_project() {
         rm -f $zip
         zip -9r $zip ./visualboyadvance-m.app
 
-        gpg --detach-sign -a $zip
+        rm -f $zip.asc
+        gpg --detach-sign -a $zip || :
     elif [ "$target_os" != windows ] && path_exists visualboyadvance-m; then
         $STRIP visualboyadvance-m
     elif [ "$target_os" = windows ] && path_exists visualboyadvance-m.exe; then
         $STRIP visualboyadvance-m.exe
+
+        mv visualboyadvance-m.exe visualboyadvance-m-unsigned.exe
+
+        osslsigncode sign -pkcs12 ~/.codesign/windows_comodo.pkcs12 -pass "$(pass vbam-windows-codesign-cert)" \
+            -n visualboyadvance-m -i https://github.com/visualboyadvance-m/visualboyadvance-m \
+            -in visualboyadvance-m-unsigned.exe -out visualboyadvance-m.exe || cp visualboyadvance-m-unsigned.exe visualboyadvance-m.exe
 
         zip=./visualboyadvance-m-Win-${target_bits:-$bits}bit.zip
 
         rm -f $zip
         zip -9 $zip ./visualboyadvance-m.exe
 
-        gpg --detach-sign -a $zip
+        rm -f translations.zip
+
+        make install DESTDIR=./destdir
+        cd destdir/usr/local/share/locale
+        zip -9r ../../../../../translations.zip *
+        cd ../../../../..
+
+        rm -f $zip.asc translations.zip.asc
+
+        gpg --detach-sign -a $zip || :
+        gpg --detach-sign -a translations.zip || :
     fi
 
     dist_post_build project

tools/builder/mingw.sh
@@ -59,8 +59,17 @@ ln -sf "$BUILD_ROOT/target" "$BUILD_ROOT/root"
 perl_dists="$perl_dists XML-NamespaceSupport XML-SAX-Base XML-SAX"
 perl_dists=$(list_remove_duplicates $perl_dists)
 
+# to codesign windows binary
+table_insert_after DISTS pkgconfig '
+    osslsigncode    https://github.com/mtrojnar/osslsigncode/archive/18810b7e0bb1d8e0d25b6c2565a065cf66bce5d7.tar.gz    bin/osslsigncode
+'
+
+table_line_append DIST_CONFIGURE_OVERRIDES osslsigncode 'sh autogen.sh && ./configure --prefix=/usr'
+
+table_line_append DIST_EXTRA_LIBS osslsigncode '-lz -lssl -lcrypto -ldl'
+
 host_dists="$host_dists autoconf autoconf-archive automake m4 gsed bison \
-                        flex-2.6.3 flex c2man docbook2x ccache ninja"
+                        flex-2.6.3 flex c2man docbook2x ccache ninja curl osslsigncode"
 host_dists=$(list_remove_duplicates $host_dists)
 
 both_dists="$both_dists openssl zlib bzip2 libiconv"

tools/osx/builder
@@ -103,7 +103,7 @@ table_line_append  DIST_PRE_BUILD       libmodplug " \
 table_line_append DIST_PRE_BUILD libzmq "sed -i.bak 's/-lstdc++/-lc++/g' src/libzmq.pc.in"
 table_line_append DIST_PRE_BUILD ffmpeg "sed -i.bak 's/-lstdc++/-lc++/g' configure"
 
-table_line_append DIST_ARGS wxwidgets  "--with-macosx-version-min=\$MACOSX_DEPLOYMENT_TARGET LDFLAGS=\"\$LDFLAGS -stdlib=libc++\""
+table_line_append DIST_CONFIGURE_OVERRIDES wxwidgets  "--with-macosx-version-min=\$MACOSX_DEPLOYMENT_TARGET LDFLAGS=\"\$LDFLAGS -stdlib=libc++\""
 table_line_append DIST_ARGS ffmpeg     "--disable-videotoolbox --extra-ldflags='-framework CoreText'"
 table_line_append DIST_ARGS libmodplug "CC=clang++ CXX=clang++"
 