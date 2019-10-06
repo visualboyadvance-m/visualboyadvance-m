abff89e4fb828ae7abe99e7beb4de770664e9529
orbea <orbea@riseup.net>
2019-10-06 08:31:26 -0700

    cmake: Use add_executable when not cross-compiling.

--- cmake/HostCompile.cmake
+++ cmake/HostCompile.cmake
@@ -1,29 +1,25 @@
 function(host_compile src dst_cmd)
-    unset(link_flags)
+    if(CMAKE_CROSSCOMPILING)
+        unset(link_flags)
 
-    if(CMAKE_HOST_WIN32)
-        if(NOT dst MATCHES "\\.[Ee][Xx][Ee]\$")
+        if(CMAKE_HOST_WIN32)
             set(dst "${dst_cmd}.exe")
+
+            if(CMAKE_COMPILER_IS_GNUCXX)
+                set(link_flags -Wl,--subsystem,console)
+            endif()
+        else()
+            set(dst "${dst_cmd}")
         endif()
 
-        if(CMAKE_COMPILER_IS_GNUCXX)
-            set(link_flags -Wl,--subsystem,console)
-        endif()
-    else()
-        set(dst "${dst_cmd}")
-    endif()
-
-    if(NOT MSVC)
         # assume cc foo.c -o foo # will work on most hosts
-        set(compile_command cc ${src} -o ${dst} ${link_flags})
+        add_custom_command(
+            OUTPUT "${dst}"
+            COMMAND cc ${src} -o ${dst} ${link_flags}
+            DEPENDS "${src}"
+        )
     else()
-        # special case for Visual Studio
-	set(compile_command ${CMAKE_C_COMPILER} ${src} /link "/out:${dst}")
+        get_filename_component(dst "${dst_cmd}" NAME)
+        add_executable("${dst}" "${src}")
     endif()
-
-    add_custom_command(
-        OUTPUT "${dst}"
-        COMMAND ${compile_command}
-        DEPENDS "${src}"
-    )
 endfunction()
