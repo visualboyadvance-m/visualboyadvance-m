49ca52ba03d4666fe358606aa9a2c9b97d8f7b83
Rafael Kitover <rkitover@gmail.com>
2019-12-15 19:34:18 +0000

    cmake: Visual Studio misc followup on cf9a88df.
    Fix the vcpkg root setting, which was trying to use CMAKE_PROJECT_DIR
    before a project is defined by switching back to CMAKE_SOURCE_DIR. Also
    check if the user has \vcpkg or c:\vcpkg and use those if found. Use
    \vcpkg if ENV{CI} is set, e.g. on Appveyor.
    
    Enable nasm for visual studio builds by using the nuget nasm2 package.
    
    In Architecture.cmake check that the vcpkg arch matches the compiler
    arch, and throw a fatal error otherwise.
    
    When using GLOB to find the nuget package directory, use the package
    name as the prefix, this is quite necessary when using multiple nuget
    packages.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- cmake/Architecture.cmake
+++ cmake/Architecture.cmake
@@ -14,13 +14,19 @@ endif()
 # and set WINARCH for windows stuff
 if(CMAKE_SYSTEM_PROCESSOR MATCHES "[xX]86|i[3-9]86|[aA][mM][dD]64")
     if(CMAKE_C_SIZEOF_DATA_PTR EQUAL 4) # 32 bit
-        if(NOT (MSVC AND CMAKE_TOOLCHAIN_FILE MATCHES vcpkg))
-            set(ASM_DEFAULT ON)
-        endif()
+        set(ASM_DEFAULT ON)
         set(X86_32 ON)
 	set(WINARCH x86)
+
+        if(DEFINED VCPKG_TARGET_TRIPLET AND (NOT VCPKG_TARGET_TRIPLET MATCHES "^x86-"))
+            message(FATAL_ERROR "ERROR: Wrong build environment architecture for VCPKG_TARGET_TRIPLET")
+        endif()
     else()
         set(AMD64 ON)
 	set(WINARCH x64)
+
+        if(DEFINED VCPKG_TARGET_TRIPLET AND (NOT VCPKG_TARGET_TRIPLET MATCHES "^x64-"))
+            message(FATAL_ERROR "ERROR: Wrong build environment architecture for VCPKG_TARGET_TRIPLET")
+        endif()
     endif()
 endif()
