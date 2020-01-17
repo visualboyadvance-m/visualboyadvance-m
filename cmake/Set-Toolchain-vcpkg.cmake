5cc1fd28b4af93ceea6951b6970abfe293dae745
Rafael Kitover <rkitover@gmail.com>
2020-01-17 21:55:26 +0000

    cmake: Wrap vcpkg code in a function.
    Wrap the main body of code in Set-Toolchain-vcpkg.cmake in the
    vcpkg_set_toolchain() function to not pollute the global variable
    namespace with helper variables.
    
    VCPKG_ROOT and toolchain variables are set in the cache.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- cmake/Set-Toolchain-vcpkg.cmake
+++ cmake/Set-Toolchain-vcpkg.cmake
@@ -1,10 +1,14 @@
-macro(vcpkg_check_git_status)
-    if(NOT vcpkg_git_status EQUAL 0)
+if(NOT DEFINED VCPKG_TARGET_TRIPLET)
+    return()
+endif()
+
+function(vcpkg_check_git_status git_status)
+    if(NOT git_status EQUAL 0)
         message(FATAL_ERROR "Error updating vcpkg from git, please make sure git for windows is installed correctly, it can be installed from Visual Studio components")
     endif()
-endmacro()
+endfunction()
 
-function(vcpkg_get_first_upgrade)
+function(vcpkg_get_first_upgrade vcpkg_exe)
     # First get the list of upgraded ports.
     execute_process(
         COMMAND ${vcpkg_exe} upgrade
@@ -18,159 +22,161 @@ function(vcpkg_get_first_upgrade)
 
     foreach(line ${upgrade_lines})
         if(${line} MATCHES "^  [* ] ")
-            string(REGEX REPLACE "^  [* ] " "" vcpkg_first_upgrade ${line})
-            set(vcpkg_first_upgrade ${vcpkg_first_upgrade} PARENT_SCOPE)
+            string(REGEX REPLACE "^  [* ] " "" first_upgrade ${line})
+            set(first_upgrade ${first_upgrade} PARENT_SCOPE)
             return()
         endif()
     endforeach()
 endfunction()
 
-if(NOT DEFINED VCPKG_TARGET_TRIPLET)
-    return()
-endif()
-
-if(NOT DEFINED ENV{VCPKG_ROOT})
-    if(WIN32)
-        if(DEFINED ENV{CI} OR EXISTS /vcpkg)
-            set(VCPKG_ROOT /vcpkg)
-        elseif(EXISTS c:/vcpkg)
-            set(VCPKG_ROOT c:/vcpkg)
+function(vcpkg_set_toolchain)
+    if(NOT DEFINED ENV{VCPKG_ROOT})
+        if(WIN32)
+            if(DEFINED ENV{CI} OR EXISTS /vcpkg)
+                set(VCPKG_ROOT /vcpkg)
+            elseif(EXISTS c:/vcpkg)
+                set(VCPKG_ROOT c:/vcpkg)
+            endif()
         endif()
+
+        if(NOT DEFINED VCPKG_ROOT)
+            get_filename_component(VCPKG_ROOT ${CMAKE_SOURCE_DIR}/../vcpkg ABSOLUTE)
+        endif()
+
+        set(ENV{VCPKG_ROOT} ${VCPKG_ROOT})
+    else()
+        set(VCPKG_ROOT $ENV{VCPKG_ROOT})
     endif()
 
-    if(NOT DEFINED VCPKG_ROOT)
-        get_filename_component(VCPKG_ROOT ${CMAKE_SOURCE_DIR}/../vcpkg ABSOLUTE)
-    endif()
+    set(VCPKG_ROOT ${VCPKG_ROOT} CACHE FILEPATH "vcpkg installation root path" FORCE)
 
-    set(ENV{VCPKG_ROOT} ${VCPKG_ROOT})
-else()
-    set(VCPKG_ROOT $ENV{VCPKG_ROOT})
-endif()
+    if(NOT EXISTS ${VCPKG_ROOT})
+        get_filename_component(root_parent ${VCPKG_ROOT}/.. ABSOLUTE)
 
-if(NOT EXISTS ${VCPKG_ROOT})
-    get_filename_component(vcpkg_root_parent ${VCPKG_ROOT}/.. ABSOLUTE)
-
-    execute_process(
-        COMMAND git clone https://github.com/microsoft/vcpkg.git
-        RESULT_VARIABLE vcpkg_git_status
-        WORKING_DIRECTORY ${vcpkg_root_parent}
-    )
-
-    vcpkg_check_git_status()
-else()
-    # this is the case when we cache vcpkg/installed with the appveyor build cache
-    if(NOT EXISTS ${VCPKG_ROOT}/.git)
-        set(git_commands
-            "git init"
-            "git remote add origin https://github.com/microsoft/vcpkg.git"
-            "git fetch --all --prune"
-            "git reset --hard origin/master"
-            "git branch --set-upstream-to=origin/master master"
+        execute_process(
+            COMMAND git clone https://github.com/microsoft/vcpkg.git
+            RESULT_VARIABLE git_status
+            WORKING_DIRECTORY ${root_parent}
         )
-        foreach(git_command ${git_commands})
-            separate_arguments(git_command)
 
+        vcpkg_check_git_status(${git_status})
+    else()
+        # this is the case when we cache vcpkg/installed with the appveyor build cache
+        if(NOT EXISTS ${VCPKG_ROOT}/.git)
+            set(git_commands
+                "git init"
+                "git remote add origin https://github.com/microsoft/vcpkg.git"
+                "git fetch --all --prune"
+                "git reset --hard origin/master"
+                "git branch --set-upstream-to=origin/master master"
+            )
+            foreach(git_command ${git_commands})
+                separate_arguments(git_command)
+
+                execute_process(
+                    COMMAND ${git_command}
+                    RESULT_VARIABLE git_status
+                    WORKING_DIRECTORY ${VCPKG_ROOT}
+                )
+
+                vcpkg_check_git_status(${git_status})
+            endforeach()
+        else()
             execute_process(
-                COMMAND ${git_command}
-                RESULT_VARIABLE vcpkg_git_status
+                COMMAND git fetch origin
+                RESULT_VARIABLE git_status
                 WORKING_DIRECTORY ${VCPKG_ROOT}
             )
+            vcpkg_check_git_status(${git_status})
 
-            vcpkg_check_git_status()
-        endforeach()
-    else()
-        execute_process(
-            COMMAND git fetch origin
-            RESULT_VARIABLE vcpkg_git_status
-            WORKING_DIRECTORY ${VCPKG_ROOT}
-        )
-        vcpkg_check_git_status()
-
-        execute_process(
-            COMMAND git status
-            RESULT_VARIABLE vcpkg_git_status
-            OUTPUT_VARIABLE git_status_text
-            WORKING_DIRECTORY ${VCPKG_ROOT}
-        )
-        vcpkg_check_git_status()
-
-        set(vcpkg_git_up_to_date FALSE)
-
-        if(git_status_text MATCHES "Your branch is up to date with")
-            set(vcpkg_git_up_to_date TRUE)
-        endif()
-
-        if(NOT vcpkg_git_up_to_date)
             execute_process(
-                COMMAND git pull --rebase
-                RESULT_VARIABLE vcpkg_git_status
+                COMMAND git status
+                RESULT_VARIABLE git_status
+                OUTPUT_VARIABLE git_status_text
                 WORKING_DIRECTORY ${VCPKG_ROOT}
             )
+            vcpkg_check_git_status(${git_status})
 
-            vcpkg_check_git_status()
+            set(git_up_to_date FALSE)
+
+            if(git_status_text MATCHES "Your branch is up to date with")
+                set(git_up_to_date TRUE)
+            endif()
+
+            if(NOT git_up_to_date)
+                execute_process(
+                    COMMAND git pull --rebase
+                    RESULT_VARIABLE git_status
+                    WORKING_DIRECTORY ${VCPKG_ROOT}
+                )
+
+                vcpkg_check_git_status(${git_status})
+            endif()
+        endif()
+
+        vcpkg_check_git_status(${git_status})
+    endif()
+
+    # build latest vcpkg, if needed
+    if(NOT git_up_to_date)
+        if(WIN32)
+            execute_process(
+                COMMAND bootstrap-vcpkg.bat
+                WORKING_DIRECTORY ${VCPKG_ROOT}
+            )
+        else()
+            execute_process(
+                COMMAND ./bootstrap-vcpkg.sh
+                WORKING_DIRECTORY ${VCPKG_ROOT}
+            )
         endif()
     endif()
 
-    vcpkg_check_git_status()
-endif()
+    foreach(pkg ${VCPKG_DEPS})
+        list(APPEND VCPKG_DEPS_QUALIFIED ${pkg}:${VCPKG_TARGET_TRIPLET})
+    endforeach()
 
-# build latest vcpkg, if needed
-if(NOT vcpkg_git_up_to_date)
     if(WIN32)
-        execute_process(
-            COMMAND bootstrap-vcpkg.bat
-            WORKING_DIRECTORY ${VCPKG_ROOT}
-        )
+        set(vcpkg_exe vcpkg)
     else()
-        execute_process(
-            COMMAND ./bootstrap-vcpkg.sh
-            WORKING_DIRECTORY ${VCPKG_ROOT}
-        )
+        set(vcpkg_exe ./vcpkg)
     endif()
-endif()
 
-foreach(pkg ${VCPKG_DEPS})
-    list(APPEND VCPKG_DEPS_QUALIFIED ${pkg}:${VCPKG_TARGET_TRIPLET})
-endforeach()
-
-if(WIN32)
-    set(vcpkg_exe vcpkg)
-else()
-    set(vcpkg_exe ./vcpkg)
-endif()
-
-# update portfiles
-execute_process(
-    COMMAND ${vcpkg_exe} update
-    WORKING_DIRECTORY ${VCPKG_ROOT}
-)
-
-# build our deps
-execute_process(
-    COMMAND ${vcpkg_exe} install ${VCPKG_DEPS_QUALIFIED}
-    WORKING_DIRECTORY ${VCPKG_ROOT}
-)
-
-# If ports have been updated, rebuild cache one at a time to not overrun the CI time limit.
-vcpkg_get_first_upgrade()
-
-if(DEFINED vcpkg_first_upgrade)
+    # update portfiles
     execute_process(
-        COMMAND ${vcpkg_exe} upgrade --no-dry-run ${vcpkg_first_upgrade}
+        COMMAND ${vcpkg_exe} update
         WORKING_DIRECTORY ${VCPKG_ROOT}
     )
-endif()
 
-if(WIN32 AND VCPKG_TARGET_TRIPLET MATCHES x64 AND CMAKE_GENERATOR MATCHES "Visual Studio")
-    set(CMAKE_GENERATOR_PLATFORM x64 CACHE STRING "visual studio build architecture" FORCE)
-endif()
+    # build our deps
+    execute_process(
+        COMMAND ${vcpkg_exe} install ${VCPKG_DEPS_QUALIFIED}
+        WORKING_DIRECTORY ${VCPKG_ROOT}
+    )
 
-if(WIN32 AND (NOT CMAKE_GENERATOR MATCHES "Visual Studio"))
-    # set toolchain to VS for e.g. Ninja or jom
-    set(CMAKE_C_COMPILER   cl CACHE STRING "Microsoft C/C++ Compiler" FORCE)
-    set(CMAKE_CXX_COMPILER cl CACHE STRING "Microsoft C/C++ Compiler" FORCE)
-endif()
+    # If ports have been updated, rebuild cache one at a time to not overrun the CI time limit.
+    vcpkg_get_first_upgrade(${vcpkg_exe})
 
-set(CMAKE_TOOLCHAIN_FILE ${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake CACHE FILEPATH "vcpkg toolchain" FORCE)
+    if(DEFINED first_upgrade)
+        execute_process(
+            COMMAND ${vcpkg_exe} upgrade --no-dry-run ${first_upgrade}
+            WORKING_DIRECTORY ${VCPKG_ROOT}
+        )
+    endif()
+
+    if(WIN32 AND VCPKG_TARGET_TRIPLET MATCHES x64 AND CMAKE_GENERATOR MATCHES "Visual Studio")
+        set(CMAKE_GENERATOR_PLATFORM x64 CACHE STRING "visual studio build architecture" FORCE)
+    endif()
+
+    if(WIN32 AND (NOT CMAKE_GENERATOR MATCHES "Visual Studio"))
+        # set toolchain to VS for e.g. Ninja or jom
+        set(CMAKE_C_COMPILER   cl CACHE STRING "Microsoft C/C++ Compiler" FORCE)
+        set(CMAKE_CXX_COMPILER cl CACHE STRING "Microsoft C/C++ Compiler" FORCE)
+    endif()
+
+    set(CMAKE_TOOLCHAIN_FILE ${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake CACHE FILEPATH "vcpkg toolchain" FORCE)
+endfunction()
+
+vcpkg_set_toolchain()
 
 include(${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake)
