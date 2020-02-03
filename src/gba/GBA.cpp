789cd2d0f0742093718b6acead76c401b4b367d3
Edênis Freindorfer Azevedo <edenisfa@gmail.com>
2020-02-03 07:38:26 -0300

    Revert "Enable layers in non-tile modes."
    We need to use proper documented behaviour when coding the
    core, therefore this should be reverted until a proper
    solution is done.
    
    This reverts commit ca3b63d64c6d3d5e4670d3d93b2479029b07ed18.

--- src/gba/GBA.cpp
+++ src/gba/GBA.cpp
@@ -577,7 +577,6 @@ extern uint32_t line3[240];
 
 void CPUUpdateRenderBuffers(bool force)
 {
-    layerEnable = layerSettings & DISPCNT;
     if (!(layerEnable & 0x0100) || force) {
         CLEAR_ARRAY(line0);
     }
