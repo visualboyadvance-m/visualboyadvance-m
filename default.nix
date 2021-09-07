7a550f43487fcefc4f2e0932ea187f787db9c9b6
Rafael Kitover <rkitover@gmail.com>
2021-09-07 23:05:31 +0000

    Update default.nix.
    Add indirect dependencies that cmake complains about and remove ccache
    because it currently fails to build on NixOS unstable.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
     name = "visualboyadvance-m";
-    buildInputs = [ ninja cmake gcc nasm gettext pkg-config ccache zip sfml zlib ffmpeg wxGTK31-gtk3 mesa glfw SDL2 gtk3-x11 ];
+    buildInputs = [ ninja cmake gcc nasm gettext pkg-config zip sfml zlib ffmpeg wxGTK31-gtk3 mesa glfw SDL2 gtk3-x11 pcre util-linuxMinimal libselinux libsepol libthai libdatrie xorg.libXdmcp xorg.libXtst libxkbcommon epoxy dbus_libs at-spi2-core ];
 }
