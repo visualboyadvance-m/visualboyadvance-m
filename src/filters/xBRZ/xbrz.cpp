94717d44a98c786275ab09be317324fcb0ec4d87
XieJiSS <XieJiSS@users.noreply.github.com>
2021-11-13 16:00:31 +0800

    fix: make vbam build on riscv

--- src/filters/xBRZ/xbrz.cpp
+++ src/filters/xBRZ/xbrz.cpp
@@ -66,7 +66,7 @@ uint32_t gradientARGB(uint32_t pixFront, uint32_t pixBack) //find intermediate c
 
 inline double fastSqrt(double n)
 {
-#if (defined(__GNUC__) || defined(__clang__)) && (defined(__x86_64__) || defined(__i386__))
+#if ((defined(__GNUC__) && !defined(__riscv)) || defined(__clang__)) && (defined(__x86_64__) || defined(__i386__))
     __asm__ ("fsqrt" : "+t" (n));
     return n;
 #elif defined(_MSC_VER) && defined(_M_IX86)
