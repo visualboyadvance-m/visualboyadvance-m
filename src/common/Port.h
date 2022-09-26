063f21dd0e839d2449dd2ebd6a1a377d05db8be3
Fabrice de Gans <steelskin@gmail.com>
2022-09-25 18:40:45 -0700

    [build] Fix all warnings in src/gba/bios.cpp
    This adds some helper methods to safely downcast integer types.
    
    Bug: #1003

--- src/common/Port.h
+++ src/common/Port.h
@@ -13,18 +13,6 @@
 #include <ppcintrinsics.h>
 #endif
 
-// swaps a 16-bit value
-static inline uint16_t swap16(uint16_t v)
-{
-        return (v << 8) | (v >> 8);
-}
-
-// swaps a 32-bit value
-static inline uint32_t swap32(uint32_t v)
-{
-        return (v << 24) | ((v << 8) & 0xff0000) | ((v >> 8) & 0xff00) | (v >> 24);
-}
-
 #ifdef WORDS_BIGENDIAN
 #if defined(__GNUC__) && defined(__ppc__)
 
@@ -47,6 +35,19 @@ static inline uint32_t swap32(uint32_t v)
 #define WRITE32LE(base, value) __asm__("stwbrx %0, 0, %1" : : "r"(value), "r"(base) : "memory")
 
 #else
+
+// swaps a 16-bit value
+static inline uint16_t swap16(uint16_t v)
+{
+        return (v << 8) | (v >> 8);
+}
+
+// swaps a 32-bit value
+static inline uint32_t swap32(uint32_t v)
+{
+        return (v << 24) | ((v << 8) & 0xff0000) | ((v >> 8) & 0xff00) | (v >> 24);
+}
+
 #define READ16LE(x) swap16(*((uint16_t *)(x)))
 #define READ32LE(x) swap32(*((uint32_t *)(x)))
 #define WRITE16LE(x, v) *((uint16_t *)x) = swap16((v))
