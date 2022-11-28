a42e166e94753ca0a8e290225ec7127a623374dd
Rafael Kitover <rkitover@gmail.com>
2022-11-28 18:34:48 +0000

    build: fix syntax and rm dbg prints from 2a65e92d
    Remove two debug messages and add a missing paren to the cmake source.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- cmake/RemoveStrawberryPerlFromPATH.cmake
+++ cmake/RemoveStrawberryPerlFromPATH.cmake
@@ -2,16 +2,12 @@
 # pollution.
 unset(new_path)
 
-message("BEFORE: $ENV{PATH}")
-
 foreach(p $ENV{PATH})
     if(NOT p MATCHES "(^|\\\\)[Ss]trawberry\\\\([Pp]erl|[Cc])\\\\(.*\\\\)?[Bb]in$")
         list(APPEND new_path ${p})
     endif()
-endforeach(
+endforeach()
 
 set(ENV{PATH} "${new_path}")
-
-message("AFTER: $ENV{PATH}")
 
 # vim:sw=4 et sts=4 ts=8:
