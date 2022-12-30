6307348cbc96f696d3b51811ae032c7998eb0089
Rafael Kitover <rkitover@gmail.com>
2022-12-30 02:30:08 +0000

    build: support MSVC arm64 cross build on x86
    Adjust cmake code to support building a Windows arm64 binary on a
    Windows x86/x64 host.
    
    Add /EHsc to MSVC compiler flags for exception unwind support.
    
    Check VCPKG_TARGET_TRIPLET in Architecture.cmake to set the appropriate
    cmake variables for cross-compiling for arm64.
    
    Add MSVC_x86_Host_Compile.cmake script, used from HostCompile.cmake, to
    adjust the path of the cl.exe compiler and ENV{LIB} to build a C program
    for x86 when cross-compiling, necessary for the bin2c utility.
    
    Use wxrc from dependencies submodule when cross-compiling, because the
    cross wxrc executable cannot be run on the host.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- cmake/Architecture.cmake
+++ cmake/Architecture.cmake
@@ -21,6 +21,12 @@ elseif(CMAKE_C_SIZEOF_DATA_PTR EQUAL 8)
     set(BITS 64)
 endif()
 
+if(VCPKG_TARGET_TRIPLET MATCHES "^[aA][rR][mM]64")
+    set(CMAKE_SYSTEM_PROCESSOR ARM64)
+elseif(VCPKG_TARGET_TRIPLET MATCHES "^[aA][rR][mM]-")
+    set(CMAKE_SYSTEM_PROCESSOR ARM)
+endif()
+
 # Turn asm on by default on 32bit x86 and set WINARCH for windows stuff.
 if(CMAKE_SYSTEM_PROCESSOR MATCHES "[xX]86|i[3-9]86|[aA][mM][dD]64")
     if(CMAKE_C_SIZEOF_DATA_PTR EQUAL 4) # 32 bit
@@ -51,5 +57,9 @@ elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "[aA][aA][rR][cC][hH]|[aA][rR][mM]")
     elseif(CMAKE_C_SIZEOF_DATA_PTR EQUAL 8)
         set(ARCH_NAME arm64)
         set(WINARCH arm64)
+    endif()
+
+    if(NOT CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "[aA][aA][rR][cC][hH]|[aA][rR][mM]")
+        set(CMAKE_CROSSCOMPILING TRUE)
     endif()
 endif()
