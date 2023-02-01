be1e5ca18c8d0435c5611d5b4599121ea2ba6dee
Rafael Kitover <rkitover@gmail.com>
2023-02-01 04:58:56 +0000

    Check for wx < 3.2.2 for EGL move fix backport
    When executing the EGL subsurface move backport fix, check for loaded
    library version < 3.2.2 so as not to execute an unnecessary EGL
    subsurface move operation on versions which will already have the fix.
    
    References #1028
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- src/wx/wayland.cpp
+++ src/wx/wayland.cpp
@@ -12,6 +12,7 @@ bool IsWayland() { return GDK_IS_WAYLAND_DISPLAY(gdk_display_get_default()); }
 #ifdef WAYLAND_MOVE_SUBSURFACE_BACKPORT
 #include <wayland-egl.h>
 #include <wx/glcanvas.h>
+#include <wx/utils.h>
 
 wl_subsurface* wxGLCanvasEGL::* private_ptr;
 
@@ -29,8 +30,16 @@ void MoveWaylandSubsurface(wxGLCanvasEGL* win)
 {
     if (!IsWayland()) return;
 
-    int x, y;
-    gdk_window_get_origin(win->GTKGetDrawingWindow(), &x, &y);
-    wl_subsurface_set_position(win->*private_ptr, x, y);
+    // Check for 3.2.2 at runtime as well, because an app may have been
+    // compiled against 3.2.0 or 3.2.1 but running with 3.2.2 after a distro
+    // upgrade.
+    auto&& wxver = wxGetLibraryVersionInfo();
+    int&& major = wxver.GetMajor(), minor = wxver.GetMinor(), micro = wxver.GetMicro();
+
+    if (!(major > 3 || (major == 3 && (minor > 2 || (minor == 2 && micro >= 2))))) {
+        int x, y;
+        gdk_window_get_origin(win->GTKGetDrawingWindow(), &x, &y);
+        wl_subsurface_set_position(win->*private_ptr, x, y);
+    }
 }
 #endif
