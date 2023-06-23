790618102dea1a4c94a115e4f3e8b432eb53adee
Rafael Kitover <rkitover@gmail.com>
2023-06-23 00:56:37 +0000

    builder: bump SDL2 2.24.0 -> 2.28.0
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- tools/builder/core.sh
+++ tools/builder/core.sh
@@ -182,7 +182,7 @@ DISTS=$DISTS'
     libgpg-error    https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.45.tar.bz2                         lib/libgpg-error.a
     libgcrypt       https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.10.1.tar.bz2                             lib/libgcrypt.a
     libsecret       https://gitlab.gnome.org/GNOME/libsecret/-/archive/0.20.5/libsecret-0.20.5.tar.bz2          lib/libsecret-1.a
-    sdl2            https://github.com/libsdl-org/SDL/releases/download/release-2.24.0/SDL2-2.24.0.tar.gz       lib/libSDL2.a
+    sdl2            https://github.com/libsdl-org/SDL/releases/download/release-2.28.0/SDL2-2.28.0.tar.gz       lib/libSDL2.a
     flac            https://ftp.osuosl.org/pub/xiph/releases/flac/flac-1.3.4.tar.xz                             lib/libFLAC.a
     libogg          http://downloads.xiph.org/releases/ogg/libogg-1.3.5.tar.xz                                  lib/libogg.a
     libvorbis       http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.7.tar.xz                            lib/libvorbis.a
