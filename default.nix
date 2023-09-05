ea5cbba0164d8bb4bb0d6f7d68768d352aa412c0
Rafael Kitover <rkitover@gmail.com>
2023-09-05 16:49:21 +0000

    build: update default.nix for macOS and new wx
    Update the default.nix, which is used on nixOS and other nix
    installations for loading the build dependencies into a shell for
    building.
    
    Update the wxWidgets package to wxGTK32, the current version.
    
    Add an if statement to check for macOS and load the right package set
    for it.
    
    Building with nix on macOS does not currently work, but the build
    dependencies do work now.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- default.nix
+++ default.nix
@@ -1,7 +1,8 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
     name = "visualboyadvance-m";
-    buildInputs = [ ninja cmake gcc nasm gettext pkg-config zip sfml zlib ffmpeg wxGTK31-gtk3 mesa glfw SDL2 gtk3-x11 pcre util-linuxMinimal libselinux libsepol libthai libdatrie xorg.libXdmcp xorg.libXtst libxkbcommon epoxy dbus at-spi2-core ];
-# WIP: Trying to get this to work on mac nix.
-#    buildInputs = [ ninja cmake gcc nasm gettext pkg-config zip sfml zlib ffmpeg wxmac SDL2 pcre ];
+    buildInputs = if stdenv.isDarwin then
+        [ ninja cmake gcc nasm gettext pkg-config zip sfml zlib ffmpeg wxGTK32 SDL2 pcre ]
+    else
+        [ ninja cmake gcc nasm gettext pkg-config zip sfml zlib ffmpeg wxGTK32 mesa glfw SDL2 gtk3-x11 pcre util-linuxMinimal libselinux libsepol libthai libdatrie xorg.libXdmcp xorg.libXtst libxkbcommon epoxy dbus at-spi2-core ];
 }
