e17d2cbaf51218f33e9f745d8332f63c844eba67
Rafael Kitover <rkitover@gmail.com>
2023-10-04 13:19:44 -0700

    build: fixes for nix on macOS
    Add references to frameworks being linked to `buildInputs` in
    `default.nix`.  Remove `-framework System` from `wxWidgets_LIBRARIES` in
    the cmake code as it's not necessary and nix does it have it in
    `darwin.apple_sdk.frameworks`.
    
    TODO:
    
    Debug build currently builds and runs. Release build builds but does not
    run because of a problem with the dylib bundling.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
     name = "visualboyadvance-m";
     buildInputs = if stdenv.isDarwin then
-        [ ninja cmake gcc nasm gettext pkg-config zip sfml zlib openal ffmpeg wxGTK32 SDL2 pcre pcre2 ]
+        [ ninja cmake gcc nasm gettext pkg-config zip sfml zlib openal ffmpeg wxGTK32 SDL2 pcre pcre2 darwin.apple_sdk.frameworks.IOKit darwin.apple_sdk.frameworks.Carbon darwin.apple_sdk.frameworks.Cocoa darwin.apple_sdk.frameworks.QuartzCore darwin.apple_sdk.frameworks.AudioToolbox darwin.apple_sdk.frameworks.OpenGL darwin.apple_sdk.frameworks.OpenAL ]
     else
         [ ninja cmake gcc nasm gettext pkg-config zip sfml zlib openal ffmpeg wxGTK32 mesa glfw SDL2 gtk3-x11 pcre pcre2 util-linuxMinimal libselinux libsepol libthai libdatrie xorg.libXdmcp xorg.libXtst libxkbcommon epoxy dbus at-spi2-core ];
 }
