7e46939826b0a72f8034c779503672d6946444ab
Rafael Kitover <rkitover@gmail.com>
2023-10-04 16:26:46 +0000

    build: add pcre2 and openal to default.nix
    Add openal, which was missing, and pcre2 which is required by glib,
    which is an indirect dependency.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
     name = "visualboyadvance-m";
     buildInputs = if stdenv.isDarwin then
-        [ ninja cmake gcc nasm gettext pkg-config zip sfml zlib ffmpeg wxGTK32 SDL2 pcre ]
+        [ ninja cmake gcc nasm gettext pkg-config zip sfml zlib openal ffmpeg wxGTK32 SDL2 pcre pcre2 ]
     else
-        [ ninja cmake gcc nasm gettext pkg-config zip sfml zlib ffmpeg wxGTK32 mesa glfw SDL2 gtk3-x11 pcre util-linuxMinimal libselinux libsepol libthai libdatrie xorg.libXdmcp xorg.libXtst libxkbcommon epoxy dbus at-spi2-core ];
+        [ ninja cmake gcc nasm gettext pkg-config zip sfml zlib openal ffmpeg wxGTK32 mesa glfw SDL2 gtk3-x11 pcre pcre2 util-linuxMinimal libselinux libsepol libthai libdatrie xorg.libXdmcp xorg.libXtst libxkbcommon epoxy dbus at-spi2-core ];
 }
