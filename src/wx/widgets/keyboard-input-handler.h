9d20ce9b597164a2f90d84843b5764ec92e1676f
Fabrice de Gans <Steelskin@users.noreply.github.com>
2024-10-08 18:22:17 -0700

    [Input] Reset keyboard tracking on focus loss (#1357)
    When pressing Alt+Tab, the "Alt" and "Tab" keys were considered in the
    "pressed" state until the user pressed them again because the window is
    no longer receiving keyboard events. This resulted in some shortcuts no
    longer working, since "Alt" was always in the pressed state. This
    changes the keyboard tracking to be reset when the application loses
    focus, fixing the issue.
    
    This change also adds tests for the keyboard tracking.

---
+++ src/wx/widgets/keyboard-input-handler.h
@@ -0,0 +1,49 @@
+#ifndef VBAM_WX_WIDGETS_KEYBOARD_INPUT_HANDLER_H_
+#define VBAM_WX_WIDGETS_KEYBOARD_INPUT_HANDLER_H_
+
+#include <unordered_set>
+
+#include <wx/event.h>
+
+#include "wx/config/user-input.h"
+#include "wx/widgets/event-handler-provider.h"
+
+namespace widgets {
+
+// Object that is used to fire user input events when a keyboard key is pressed
+// or released. This class should be kept as a singleton owned by the
+// application object. It is meant to be used in the FilterEvent() method of the
+// app to create user input events globally whenever the keyboard is used.
+class KeyboardInputHandler final {
+public:
+    explicit KeyboardInputHandler(EventHandlerProvider* const handler_provider);
+    ~KeyboardInputHandler();
+
+    // Disable copy and copy assignment.
+    KeyboardInputHandler(const KeyboardInputHandler&) = delete;
+    KeyboardInputHandler& operator=(const KeyboardInputHandler&) = delete;
+
+    // Processes the provided key event and sends the appropriate user input
+    // event to the current event handler.
+    void ProcessKeyEvent(wxKeyEvent& event);
+
+    // Resets the state of the sender. This should be called when the main frame
+    // loses focus to prevent stuck keys.
+    void Reset();
+
+private:
+    // Keyboard event handlers.
+    void OnKeyDown(wxKeyEvent& event);
+    void OnKeyUp(wxKeyEvent& event);
+
+    std::unordered_set<wxKeyCode> active_keys_;
+    std::unordered_set<wxKeyModifier> active_mods_;
+    std::unordered_set<config::KeyboardInput> active_mod_inputs_;
+
+    // The provider of event handlers to send the events to.
+    EventHandlerProvider* const handler_provider_;
+};
+
+}  // namespace widgets
+
+#endif  // VBAM_WX_WIDGETS_KEYBOARD_INPUT_HANDLER_H_
