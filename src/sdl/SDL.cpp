29e534c733c44cdff4087a5467baaccc3012f8bc
Andy Vandijck <andyvand@gmail.com>
2025-04-04 09:54:29 +0200

    Add SDL version print
    Add SDL version print

--- src/sdl/SDL.cpp
+++ src/sdl/SDL.cpp
@@ -994,10 +994,11 @@ void sdlInitVideo()
 #if !defined(CONFIG_IDF_TARGET) && !defined(NO_OPENGL)
     if (openGL)
     {
-        systemMessage(0, "Renderer: OpenGL %s\n", openGL == 2 ? "bilinear" : "no filter");
+        systemMessage(0, "Renderer: OpenGL %s", openGL == 2 ? "bilinear" : "nearest");
     } else {
 #endif
-        systemMessage(0, "Renderer: %s\n", SDL_GetRendererName(renderer));
+    	systemMessage(0, "Renderer: SDL %d.%d (%s)", SDL_MAJOR_VERSION, SDL_MINOR_VERSION, SDL_GetRendererName(renderer));
+
 #if !defined(CONFIG_IDF_TARGET) && !defined(NO_OPENGL)
     }
 #endif
