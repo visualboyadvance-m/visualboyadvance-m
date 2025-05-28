c048f0c7133b81cf3810847190ebca3b1190e791
Andy Vandijck <andyvand@gmail.com>
2025-05-28 13:08:57 +0200

    Fix find ffmpeg
    Fix find ffmpeg

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -62,6 +62,8 @@ else()
 endif()
 
 if(ENABLE_FFMPEG)
+    include(FindFFmpeg)
+
     if(NOT FFMPEG_LIBRARIES)
         message(FATAL_ERROR "ENABLE_FFMPEG was specified, but required versions of ffmpeg libraries cannot be found!")
     endif()
