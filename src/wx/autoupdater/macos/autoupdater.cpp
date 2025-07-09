3d8ed512cf1f4bffdc078326574830a21d92c412
Andy Vandijck <andyvand@gmail.com>
2025-07-09 16:13:22 +0200

    Use macOS 10.10 Sparkle

--- src/wx/autoupdater/macos/autoupdater.cpp
+++ src/wx/autoupdater/macos/autoupdater.cpp
@@ -2,21 +2,17 @@
 
 #include "wx/autoupdater/macos/sparkle-wrapper.h"
 
-extern bool is_macosx_1013_or_newer();
-
 SparkleWrapper autoupdater;
 
 void initAutoupdater()
 {
-    if (is_macosx_1013_or_newer())
-        autoupdater.addAppcastURL("https://data.visualboyadvance-m.org/appcast.xml");
+    autoupdater.addAppcastURL("https://data.visualboyadvance-m.org/appcast.xml");
 }
 
 
 void checkUpdatesUi()
 {
-    if (is_macosx_1013_or_newer())
-        autoupdater.checkForUpdatesUi();
+    autoupdater.checkForUpdatesUi();
 }
 
 
