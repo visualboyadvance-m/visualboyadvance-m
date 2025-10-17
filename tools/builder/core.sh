704f2f19db20601c1fd3596cfb9d4d4db59f729f
Rafael Kitover <rkitover@gmail.com>
2025-10-17 13:36:13 -0700

    build: turn off tests for openssl for Mac builder
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- tools/builder/core.sh
+++ tools/builder/core.sh
@@ -322,7 +322,7 @@ DIST_POST_BUILD="$DIST_POST_BUILD
 DIST_CONFIGURE_OVERRIDES="$DIST_CONFIGURE_OVERRIDES
     ccache      cmake -G Ninja $CMAKE_INSTALL_ARGS -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 -S .. -B .
     zlib-ng        ./configure --prefix=/usr --static --zlib-compat
-    openssl     ./config no-shared --prefix=/usr --openssldir=/etc/ssl
+    openssl     ./config no-shared no-unit-test --prefix=/usr --openssldir=/etc/ssl
     cmake       ./configure --prefix=/usr --no-qt-gui --parallel=\$NUM_CPUS
     XML-SAX     echo no | PERL_MM_USE_DEFAULT=0 \"\$perl\" Makefile.PL
     libvpx      ./configure --disable-shared --enable-static --prefix=/usr --disable-unit-tests --disable-tools --disable-docs --disable-examples
