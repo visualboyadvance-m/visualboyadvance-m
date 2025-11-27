ed4df1ea77f11de54544fd01308fa8d227bda072
Squall-Leonhart <danialhorton@hotmail.com>
2025-11-28 08:25:37 +1100

    the notifier was inaccessible,
    fix it so its not

--- src/wx/audio/internal/xaudio2.h
+++ src/wx/audio/internal/xaudio2.h
@@ -5,8 +5,48 @@
 #include <memory>
 #include <vector>
 
+// Windows headers for IMMNotificationClient
+#include <windows.h>
+#include <mmdeviceapi.h>
+
 namespace audio {
 namespace internal {
+
+// XAudio2_Output interface for device change notifications
+class XAudio2_Output {
+public:
+    virtual void device_change() = 0;
+    virtual ~XAudio2_Output() = default;
+};
+
+// Device Notifier for hotplug support
+class XAudio2_Device_Notifier : public IMMNotificationClient {
+    volatile LONG registered;
+    IMMDeviceEnumerator* pEnumerator;
+    std::wstring last_device;
+    CRITICAL_SECTION lock;
+    std::vector<XAudio2_Output*> instances;
+
+public:
+    XAudio2_Device_Notifier();
+    ~XAudio2_Device_Notifier();
+
+    ULONG STDMETHODCALLTYPE AddRef() override;
+    ULONG STDMETHODCALLTYPE Release() override;
+    HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, VOID** ppvInterface) override;
+
+    HRESULT STDMETHODCALLTYPE OnDefaultDeviceChanged(EDataFlow flow, ERole role, LPCWSTR pwstrDeviceId) override;
+    HRESULT STDMETHODCALLTYPE OnDeviceAdded(LPCWSTR pwstrDeviceId) override;
+    HRESULT STDMETHODCALLTYPE OnDeviceRemoved(LPCWSTR pwstrDeviceId) override;
+    HRESULT STDMETHODCALLTYPE OnDeviceStateChanged(LPCWSTR pwstrDeviceId, DWORD dwNewState) override;
+    HRESULT STDMETHODCALLTYPE OnPropertyValueChanged(LPCWSTR pwstrDeviceId, const PROPERTYKEY key) override;
+
+    void do_register(XAudio2_Output* p_instance);
+    void do_unregister(XAudio2_Output* p_instance);
+};
+
+// Global notifier instance declaration
+extern XAudio2_Device_Notifier g_notifier;
 
 // Returns the set of XAudio2 devices.
 std::vector<AudioDevice> GetXAudio2Devices();
