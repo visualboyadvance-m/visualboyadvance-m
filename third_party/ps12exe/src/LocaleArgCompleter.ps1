ab5cacb830efcc38128ca3d2565c5b1f899ff720
steve02081504 <steve02081504@users.noreply.github.com>
2026-09-19 11:16:38 +0800

    third_party/ps12exe: trim vendored copy to the files needed to build zip.exe

--- third_party/ps12exe/src/LocaleArgCompleter.ps1
+++
@@ -1,8 +0,0 @@
-﻿Param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
-Get-ChildItem $PSScriptRoot\Locale -Filter *.fbs | ForEach-Object { $_.BaseName } | Where-Object {
-	$_ -like "$($wordToComplete.Trim('"', "'"))*"
-} | ForEach-Object {
-	if ($wordToComplete.StartsWith('"')) { "`"$_`"" }
-	elseif ($wordToComplete.StartsWith("'")) { "'$_'" }
-	else { $_ }
-}
